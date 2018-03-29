481de6551ccfedac402c3fb68e3fba19fa9daf11
Michael Naumov <mnaoumov@gmail.com>
2018-03-29 16:53:27 +0300

    Implement 003 solution

--- LeetCode/LeetCode/Problem003_LongestSubstringWithoutRepeatingCharacters/BruteForce.cs
+++ LeetCode/LeetCode/Problem003_LongestSubstringWithoutRepeatingCharacters/BruteForce.cs
@@ -1,10 +1,26 @@
-﻿namespace LeetCode.Problem003_LongestSubstringWithoutRepeatingCharacters
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problem003_LongestSubstringWithoutRepeatingCharacters
 {
     public class BruteForce : ISolution
     {
         public int LengthOfLongestSubstring(string s)
         {
-            throw new System.NotImplementedException();
+            var maxLength = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                var chars = new HashSet<char>();
+                for (int j = i; j < s.Length; j++)
+                {
+                    if (chars.Add(s[j]))
+                        maxLength = Math.Max(maxLength, j - i + 1);
+                    else
+                        break;
+                }
+            }
+
+            return maxLength;
         }
     }
 }

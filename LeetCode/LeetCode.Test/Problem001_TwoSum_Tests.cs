0b2f5b944ad24e7ef006dd92a1291fb67a802876
Michael Naumov <mnaoumov@gmail.com>
2018-03-29 15:42:47 +0300

    Extract interface and test for implementation

--- LeetCode/LeetCode.Test/Problem001_TwoSum_Tests.cs
+++ LeetCode/LeetCode.Test/Problem001_TwoSum_Tests.cs
@@ -3,14 +3,15 @@ using NUnit.Framework;
 
 namespace LeetCode.Test
 {
-    public class Problem001_TwoSum_Tests
+    [TestFixture(typeof(BruteForce))]
+    public class Problem001_TwoSum_Tests<TSolution> where TSolution : ISolution, new()
     {
         [Test]
         [TestCase(new[] { 2, 7, 11, 15 }, 9, ExpectedResult = new[] { 0, 1 })]
         [TestCase(new[] { 3, 2, 4 }, 6, ExpectedResult = new[] { 1, 2 })]
         public int[] TwoSum(int[] nums, int target)
         {
-            var solution = new Solution();
+            var solution = new TSolution();
             return solution.TwoSum(nums, target);
         }
     }

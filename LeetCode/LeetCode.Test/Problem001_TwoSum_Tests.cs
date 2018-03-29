5be671546d01b285a87a467889860c29a105cb52
Michael Naumov <mnaoumov@gmail.com>
2018-03-29 15:29:33 +0300

    Add more test cases

--- LeetCode/LeetCode.Test/Problem001_TwoSum_Tests.cs
+++ LeetCode/LeetCode.Test/Problem001_TwoSum_Tests.cs
@@ -7,6 +7,7 @@ namespace LeetCode.Test
     {
         [Test]
         [TestCase(new[] { 2, 7, 11, 15 }, 9, ExpectedResult = new[] { 0, 1 })]
+        [TestCase(new[] { 3, 2, 4 }, 6, ExpectedResult = new[] { 1, 2 })]
         public int[] TwoSum(int[] nums, int target)
         {
             var solution = new Solution();

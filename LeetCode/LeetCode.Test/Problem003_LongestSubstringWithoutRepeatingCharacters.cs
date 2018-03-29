0ae6a4327a4be2f965ae16816d7780953f5dc636
Michael Naumov <mnaoumov@gmail.com>
2018-03-29 16:49:43 +0300

    Add empty for problem 3

---
+++ LeetCode/LeetCode.Test/Problem003_LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.Problem003_LongestSubstringWithoutRepeatingCharacters;
+using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+    [TestFixture(typeof(BruteForce))]
+    public class Problem003_LongestSubstringWithoutRepeatingCharacters_Tests<TSolution> where TSolution : ISolution, new()
+    {
+        [Test]
+        [TestCase("abcabcbb", ExpectedResult = 3)]
+        [TestCase("bbbbb", ExpectedResult = 1)]
+        [TestCase("pwwkew", ExpectedResult = 3)]
+        public int Test(string s)
+        {
+            return new TSolution().LengthOfLongestSubstring(s);
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Problem003_LongestSubstringWithoutRepeatingCharacters/BruteForce.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problem003_LongestSubstringWithoutRepeatingCharacters
+{
+    public class BruteForce : ISolution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

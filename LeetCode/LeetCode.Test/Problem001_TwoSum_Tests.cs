a29acd05ee23b7601eb348734e08fac413275baf
Michael Naumov <mnaoumov@gmail.com>
2018-03-29 15:27:16 +0300

    Implement first task

---
+++ LeetCode/LeetCode.Test/Problem001_TwoSum_Tests.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Problem001_TwoSum;
+using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+    public class Problem001_TwoSum_Tests
+    {
+        [Test]
+        [TestCase(new[] { 2, 7, 11, 15 }, 9, ExpectedResult = new[] { 0, 1 })]
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var solution = new Solution();
+            return solution.TwoSum(nums, target);
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Problem001_TwoSum/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode.Problem001_TwoSum
+{
+    /*
+
+https://leetcode.com/problems/two-sum/description/
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+     */
+
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                        return new[] { i, j };
+                }
+            }
+
+            throw new ArgumentException();
+        }
+    }
+}

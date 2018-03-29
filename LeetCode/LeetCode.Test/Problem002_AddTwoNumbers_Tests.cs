bb9fe9e83ee617be26af944099ad8e479c1b226f
Michael Naumov <mnaoumov@gmail.com>
2018-03-29 16:14:35 +0300

    Add Problem002 Add Two Numbers

---
+++ LeetCode/LeetCode.Test/Problem002_AddTwoNumbers_Tests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using LeetCode.Problem002_AddTwoNumbers;
+using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+    [TestFixture(typeof(Solution))]
+    public class Problem002_AddTwoNumbers_Tests<TSolution> where TSolution : ISolution, new()
+    {
+        [Test]
+        [TestCase(new[] { 2, 4, 3 }, new[] { 5, 6, 4 }, new[] { 7, 0, 8 })]
+        public void Test(int[] l1Values, int[] l2Values, int[] expectedResultValues)
+        {
+            var solution = new TSolution();
+            var result = solution.AddTwoNumbers(CreateNode(l1Values), CreateNode(l2Values));
+            Assert.That(ExtractValues(result), Is.EquivalentTo(expectedResultValues));
+        }
+
+        private static int[] ExtractValues(ListNode listNode)
+        {
+            var values = new List<int>();
+            while (listNode != null)
+            {
+                values.Add(listNode.val);
+                listNode = listNode.next;
+            }
+
+            return values.ToArray();
+        }
+
+        private static ListNode CreateNode(int[] values)
+        {
+            ListNode lastNode = null;
+            ListNode firstNode = null;
+            foreach (var value in values)
+            {
+                var node = new ListNode(value);
+                if (lastNode != null)
+                    lastNode.next = node;
+                else
+                    firstNode = node;
+                lastNode = node;
+            }
+
+            return firstNode;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Problem002_AddTwoNumbers/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problem002_AddTwoNumbers
+{
+    public class Solution : ISolution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            return l1;
+        }
+    }
+}

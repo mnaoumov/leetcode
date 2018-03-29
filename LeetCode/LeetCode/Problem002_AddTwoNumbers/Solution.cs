efd0edc6b66c46d1bcf154362aed581886b1a7c9
Michael Naumov <mnaoumov@gmail.com>
2018-03-29 16:26:22 +0300

    Implement 002

--- LeetCode/LeetCode/Problem002_AddTwoNumbers/Solution.cs
+++ LeetCode/LeetCode/Problem002_AddTwoNumbers/Solution.cs
@@ -4,7 +4,24 @@
     {
         public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
         {
-            return l1;
+            var fakeHeadNode = new ListNode(0);
+            bool hasCarry = false;
+            var lastNode = fakeHeadNode;
+
+            while (l1 != null || l2 != null || hasCarry)
+            {
+                var sum = (l1?.val ?? 0) + (l2?.val ?? 0) + (hasCarry ? 1 : 0);
+                l1 = l1?.next;
+                l2 = l2?.next;
+                hasCarry = sum >= 10;
+                if (hasCarry)
+                    sum -= 10;
+
+                lastNode.next = new ListNode(sum);
+                lastNode = lastNode.next;
+            }
+
+            return fakeHeadNode.next;
         }
     }
 }

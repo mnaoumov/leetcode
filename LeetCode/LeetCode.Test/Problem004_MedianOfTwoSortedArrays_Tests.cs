b53639b9908309e5b220a1e8737851004ce82587
Michael Naumov <mnaoumov@gmail.com>
2018-03-29 19:12:00 +0300

    Add failing test

--- LeetCode/LeetCode.Test/Problem004_MedianOfTwoSortedArrays_Tests.cs
+++ LeetCode/LeetCode.Test/Problem004_MedianOfTwoSortedArrays_Tests.cs
@@ -9,6 +9,7 @@ namespace LeetCode.Test
         [Test]
         [TestCase(new[] { 1, 3 }, new[] { 2 }, ExpectedResult = 2.0)]
         [TestCase(new[] { 1, 2 }, new[] { 3, 4 }, ExpectedResult = 2.5)]
+        [TestCase(new[] { 1, 2 }, new[] { 3, 4, 5 }, ExpectedResult = 3.0)]
         public double Test(int[] nums1, int[] nums2)
         {
             return new TSolution().FindMedianSortedArrays(nums1, nums2);

4f6dcae31d4357435efa966b39e95d8253a901d7
Michael Naumov <mnaoumov@gmail.com>
2018-03-29 17:29:52 +0300

    Add placeholder for 004

---
+++ LeetCode/LeetCode.Test/Problem004_MedianOfTwoSortedArrays_Tests.cs
@@ -0,0 +1,17 @@
+﻿using LeetCode.Problem004_MedianOfTwoSortedArrays;
+using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+    [TestFixture(typeof(Solution))]
+    public class Problem004_MedianOfTwoSortedArrays_Tests<TSolution> where TSolution : ISolution, new()
+    {
+        [Test]
+        [TestCase(new[] { 1, 3 }, new[] { 2 }, ExpectedResult = 2.0)]
+        [TestCase(new[] { 1, 2 }, new[] { 3, 4 }, ExpectedResult = 2.5)]
+        public double Test(int[] nums1, int[] nums2)
+        {
+            return new TSolution().FindMedianSortedArrays(nums1, nums2);
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Problem004_MedianOfTwoSortedArrays/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problem004_MedianOfTwoSortedArrays
+{
+    public class Solution : ISolution
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

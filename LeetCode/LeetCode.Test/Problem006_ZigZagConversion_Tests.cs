746539e8d372518525b4233790cdd99f9d3ae8d0
Michael Naumov <mnaoumov@gmail.com>
2018-04-02 13:26:55 +0300

    Add empty problem 6 zig zag

---
+++ LeetCode/LeetCode.Test/Problem006_ZigZagConversion_Tests.cs
@@ -0,0 +1,16 @@
+﻿using LeetCode.Problem006_ZigZagConversion;
+using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+    [TestFixture(typeof(Solution))]
+    public class Problem006_ZigZagConversion_Tests<TSolution> where TSolution : ISolution, new()
+    {
+        [Test]
+        [TestCase("PAYPALISHIRING", 3, ExpectedResult = "PAHNAPLSIIGYIR")]
+        public string Test(string s, int numRows)
+        {
+            return new TSolution().Convert(s, numRows);
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Problem006_ZigZagConversion/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problem006_ZigZagConversion
+{
+    public class Solution : ISolution
+    {
+        public string Convert(string s, int numRows)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

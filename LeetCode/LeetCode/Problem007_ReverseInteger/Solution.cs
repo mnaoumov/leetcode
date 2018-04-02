8fb488ce5df444ae4db659f6a486683812deda8e
Michael Naumov <mnaoumov@gmail.com>
2018-04-02 15:06:06 +0300

    Add basic implementation

--- LeetCode/LeetCode/Problem007_ReverseInteger/Solution.cs
+++ LeetCode/LeetCode/Problem007_ReverseInteger/Solution.cs
@@ -1,10 +1,24 @@
-﻿namespace LeetCode.Problem007_ReverseInteger
+﻿using System;
+
+namespace LeetCode.Problem007_ReverseInteger
 {
     public class Solution : ISolution
     {
         public int Reverse(int x)
         {
-            throw new System.NotImplementedException();
+            var sign = Math.Sign(x);
+            x = Math.Abs(x);
+            var result = 0;
+            while (x > 0)
+            {
+                var lastDigit = x % 10;
+                result = result * 10 + lastDigit;
+                x = x / 10;
+            }
+
+            result = result * sign;
+
+            return result;
         }
     }
 }

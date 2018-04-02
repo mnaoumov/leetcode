a2873d999115050a5419b80fd27158aaee38e442
Michael Naumov <mnaoumov@gmail.com>
2018-04-02 15:00:05 +0300

    Add blank problem 7

---
+++ LeetCode/LeetCode.Test/Problem007_ReverseInteger_Tests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.Problem007_ReverseInteger;
+using NUnit.Framework;
+
+namespace LeetCode.Test
+{
+    [TestFixture(typeof(Solution))]
+    public class Problem007_ReverseInteger_Tests<TSolution> where TSolution : ISolution, new()
+    {
+        [Test]
+        [TestCase(123, ExpectedResult = 321)]
+        [TestCase(-123, ExpectedResult = -321)]
+        [TestCase(120, ExpectedResult = 21)]
+        [Timeout(100)]
+        public int Test(int x)
+        {
+            return new TSolution().Reverse(x);
+        }
+    }
+}
---
+++ LeetCode/LeetCode/Problem007_ReverseInteger/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problem007_ReverseInteger
+{
+    public class Solution : ISolution
+    {
+        public int Reverse(int x)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

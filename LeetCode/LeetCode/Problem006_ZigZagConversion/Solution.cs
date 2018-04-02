e875836728450916fc43dd7bc41df3a72e2ff769
Michael Naumov <mnaoumov@gmail.com>
2018-04-02 14:43:23 +0300

    Add impl

--- LeetCode/LeetCode/Problem006_ZigZagConversion/Solution.cs
+++ LeetCode/LeetCode/Problem006_ZigZagConversion/Solution.cs
@@ -1,10 +1,32 @@
-﻿namespace LeetCode.Problem006_ZigZagConversion
+﻿using System.Text;
+
+namespace LeetCode.Problem006_ZigZagConversion
 {
     public class Solution : ISolution
     {
         public string Convert(string s, int numRows)
         {
-            throw new System.NotImplementedException();
+            var period = 2 * numRows - 2;
+
+            var sb = new StringBuilder();
+
+            for (int i = 0; i < numRows; i++)
+            {
+                var j = period - i;
+                var skipJ = i == 0 || i == numRows - 1;
+                var nextI = i;
+                var nextJ = j;
+                while (nextI < s.Length)
+                {
+                    sb.Append(s[nextI]);
+                    if (!skipJ && nextJ < s.Length)
+                        sb.Append(s[nextJ]);
+                    nextI += period;
+                    nextJ += period;
+                }
+            }
+
+            return sb.ToString();
         }
     }
 }

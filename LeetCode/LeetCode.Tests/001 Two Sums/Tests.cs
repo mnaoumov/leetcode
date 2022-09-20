7ea3c6768c1551ba564467f64320a60233798ac0
Michael Naumov <mnaoumov@gmail.com>
2022-09-20 12:40:49 -0600

    001 Two Sum

---
+++ LeetCode/LeetCode.Tests/001 Two Sums/Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode._001_Two_Sums;
+using NUnit.Framework;
+
+namespace LeetCode.Tests._001_Two_Sums;
+
+public class Tests
+{
+    [Test]
+    public void Example1()
+    {
+        var result = new Solution().TwoSum(new[] { 2, 7, 11, 15 }, 9);
+        Assert.That(result, Is.EqualTo(new[] { 0, 1 }));
+    }
+
+    [Test]
+    public void Example2()
+    {
+        var result = new Solution().TwoSum(new[] { 3, 2, 4 }, 6);
+        Assert.That(result, Is.EqualTo(new[] { 1, 2 }));
+    }
+
+    [Test]
+    public void Example3()
+    {
+        var result = new Solution().TwoSum(new[] { 3, 3 }, 6);
+        Assert.That(result, Is.EqualTo(new[] { 0, 1 }));
+    }
+}
---
+++ LeetCode/LeetCode/001 Two Sums/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCode._001_Two_Sums;
+
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
+        return new[] {0, 1};
+    }
+}

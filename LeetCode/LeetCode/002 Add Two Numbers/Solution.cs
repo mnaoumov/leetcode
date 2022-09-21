4d356450fa977c8b576fcc8502895e19bb78cb19
Michael Naumov <mnaoumov@gmail.com>
2022-09-21 13:04:08 -0600

    002 Solution

--- LeetCode/LeetCode/002 Add Two Numbers/Solution.cs
+++ LeetCode/LeetCode/002 Add Two Numbers/Solution.cs
@@ -4,6 +4,33 @@ public class Solution
 {
     public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
     {
-        return l1;
+        ListNode? result = null;
+        var carry = false;
+        var listNode = new ListNode();
+        while (l1 != null || l2 != null || carry)
+        {
+            var value = (l1?.val ?? 0) + (l2?.val ?? 0);
+            if (carry)
+            {
+                value += 1;
+            }
+
+            carry = value >= 10;
+
+            if (carry)
+            {
+                value -= 10;
+            }
+
+            listNode.next = new ListNode(value);
+            listNode = listNode.next;
+
+            result ??= listNode;
+
+            l1 = l1?.next;
+            l2 = l2?.next;
+        }
+
+        return result!;
     }
 }

57daca9229e16e9cdc7ac1ad65faded303da2406
Michael Naumov <mnaoumov@gmail.com>
2022-09-23 23:13:17 -0600

    03, 04

---
+++ LeetCode/LeetCode.Tests/003 Longest Substring Without Repeating Characters/Tests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode._003_Longest_Substring_Without_Repeating_Characters;
+using NUnit.Framework;
+
+namespace LeetCode.Tests._003_Longest_Substring_Without_Repeating_Characters;
+
+public class Tests
+{
+    [Test]
+    public void Example1()
+    {
+        Assert.That(new Solution().LengthOfLongestSubstring("abcabcbb"), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void Example2()
+    {
+        Assert.That(new Solution().LengthOfLongestSubstring("bbbbb"), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Example3()
+    {
+        Assert.That(new Solution().LengthOfLongestSubstring("pwwkew"), Is.EqualTo(3));
+    }
+}
---
+++ LeetCode/LeetCode.Tests/004 Median of Two Sorted Arrays/Tests.cs
@@ -0,0 +1,31 @@
+﻿using LeetCode._004_Median_of_Two_Sorted_Arrays;
+using NUnit.Framework;
+
+namespace LeetCode.Tests._004_Median_of_Two_Sorted_Arrays;
+
+public class Tests
+{
+    [Test]
+    public void Example1()
+    {
+        Assert.That(new Solution().FindMedianSortedArrays(new[] { 1, 3 }, new[] { 2 }), Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Example2()
+    {
+        Assert.That(new Solution().FindMedianSortedArrays(new[] { 1, 2 }, new[] { 3, 4 }), Is.EqualTo(2.5));
+    }
+
+    [Test]
+    public void Empty()
+    {
+        Assert.That(new Solution().FindMedianSortedArrays(new int[] { }, new[] { 1 }), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.That(new Solution().FindMedianSortedArrays(new[] { 3 }, new[] { -2, -1 }), Is.EqualTo(-1));
+    }
+}

68827eb6edbc918f6fd74a4b6d7371eff7f2a34d
Michael Naumov <mnaoumov@gmail.com>
2022-09-24 18:17:26 -0600

    007

---
+++ LeetCode/LeetCode.Tests/007 Reverse Integer/Tests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode._007_Reverse_Integer;
+using NUnit.Framework;
+
+namespace LeetCode.Tests._007_Reverse_Integer;
+
+public class Tests
+{
+    [Test]
+    public void Example1()
+    {
+        Assert.That(new Solution().Reverse(123), Is.EqualTo(321));
+    }
+
+    [Test]
+    public void Example2()
+    {
+        Assert.That(new Solution().Reverse(-123), Is.EqualTo(-321));
+    }
+
+    [Test]
+    public void Example3()
+    {
+        Assert.That(new Solution().Reverse(120), Is.EqualTo(21));
+    }
+}
---
+++ LeetCode/LeetCode/007 Reverse Integer/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode._007_Reverse_Integer;
+
+public class Solution
+{
+    public int Reverse(int x)
+    {
+        if (x == int.MinValue)
+        {
+            return 0;
+        }
+
+        var sign = 1;
+
+        if (x < 0)
+        {
+            x = -x;
+            sign = -1;
+        }
+
+        var reverseStr = new string(x.ToString().ToCharArray().Reverse().ToArray());
+
+        if (int.TryParse(reverseStr, out var result))
+        {
+            return sign * result;
+        }
+
+        return 0;
+    }
+}

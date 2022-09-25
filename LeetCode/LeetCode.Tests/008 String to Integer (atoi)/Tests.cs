e44738340246151f90c21346721abdca766c976f
Michael Naumov <mnaoumov@gmail.com>
2022-09-24 18:55:20 -0600

    008

---
+++ LeetCode/LeetCode.Tests/008 String to Integer (atoi)/Tests.cs
@@ -0,0 +1,25 @@
+﻿using LeetCode._008_String_to_Integer__atoi_;
+using NUnit.Framework;
+
+namespace LeetCode.Tests._008_String_to_Integer__atoi_;
+
+public class Tests
+{
+    [Test]
+    public void Example1()
+    {
+        Assert.That(new Solution().MyAtoi("42"), Is.EqualTo(42));
+    }
+
+    [Test]
+    public void Example2()
+    {
+        Assert.That(new Solution().MyAtoi("   -42"), Is.EqualTo(-42));
+    }
+
+    [Test]
+    public void Example3()
+    {
+        Assert.That(new Solution().MyAtoi("4193 with words"), Is.EqualTo(4193));
+    }
+}
---
+++ LeetCode/LeetCode/008 String to Integer (atoi)/Solution.cs
@@ -0,0 +1,82 @@
+﻿namespace LeetCode._008_String_to_Integer__atoi_;
+
+public class Solution
+{
+    public int MyAtoi(string s)
+    {
+        var stage = ParsingStage.Start;
+
+        var sign = 1;
+        var result = 0;
+
+        for (var index = 0; index < s.Length; index++)
+        {
+            var symbol = s[index];
+            switch (stage)
+            {
+                case ParsingStage.Start:
+                    switch (symbol)
+                    {
+                        case ' ':
+                            continue;
+                        case '+':
+                            sign = 1;
+                            stage = ParsingStage.Sign;
+                            break;
+                        case '-':
+                            sign = -1;
+                            stage = ParsingStage.Sign;
+                            break;
+                        case { } when char.IsDigit(symbol):
+                            stage = ParsingStage.Digit;
+                            index--;
+                            break;
+                        default:
+                            return 0;
+                    }
+
+                    break;
+                case ParsingStage.Sign:
+                    if (char.IsDigit(symbol))
+                    {
+                        stage = ParsingStage.Digit;
+                        index--;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                    break;
+                case ParsingStage.Digit:
+                    if (char.IsDigit(symbol))
+                    {
+                        var digit = symbol - '0';
+                        const int maxPossibleNumberBeforeAddingLastDigit = int.MaxValue / 10;
+                        if (result is > -maxPossibleNumberBeforeAddingLastDigit and < maxPossibleNumberBeforeAddingLastDigit || result == maxPossibleNumberBeforeAddingLastDigit && digit <= 7 || result == -maxPossibleNumberBeforeAddingLastDigit && digit <= 8)
+                        {
+                            result = 10 * result + digit * sign;
+                        }
+                        else
+                        {
+                            return sign == 1 ? int.MaxValue : int.MinValue;
+                        }
+                    }
+                    else
+                    {
+                        return result;
+                    }
+
+                    break;
+            }
+        }
+
+        return result;
+    }
+
+    private enum ParsingStage
+    {
+        Start,
+        Sign,
+        Digit
+    }
+}

f219f5c2a985352620986bb3dabbb760c70c9f5a
Michael Naumov <mnaoumov@gmail.com>
2022-09-25 08:12:20 -0600

    010

---
+++ LeetCode/LeetCode.Tests/010 Regular Expression Matching/Tests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode._010_Regular_Expression_Matching;
+using NUnit.Framework;
+
+namespace LeetCode.Tests._010_Regular_Expression_Matching;
+
+[TestFixtureSource(nameof(Solutions))]
+public class Tests
+{
+    private readonly ISolution _solution;
+
+    public Tests(ISolution solution)
+    {
+        _solution = solution;
+    }
+
+    [Test]
+    public void Example1()
+    {
+        Assert.That(_solution.IsMatch("aa", "a"), Is.False);
+    }
+
+    [Test]
+    public void Example2()
+    {
+        Assert.That(_solution.IsMatch("aa", "a*"), Is.True);
+    }
+
+    [Test]
+    public void Example3()
+    {
+        Assert.That(_solution.IsMatch("ab", ".*"), Is.True);
+    }
+
+    private static IEnumerable<ISolution> Solutions
+    {
+        get
+        {
+            yield return new Solution();
+        }
+    }
+}
---
+++ LeetCode/LeetCode/010 Regular Expression Matching/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._010_Regular_Expression_Matching;
+
+public interface ISolution
+{
+    public bool IsMatch(string s, string p);
+}

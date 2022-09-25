ca30bdd5b4299d024833b15acecc0f4dbeb9531b
Michael Naumov <mnaoumov@gmail.com>
2022-09-24 20:33:35 -0600

    009

---
+++ LeetCode/LeetCode.Tests/009 Palindrome Number/Tests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode._009_Palindrome_Number;
+using NUnit.Framework;
+
+namespace LeetCode.Tests._009_Palindrome_Number;
+
+[TestFixtureSource(nameof(Solutions))]
+public class Tests
+{
+    private readonly ISolution _solution;
+
+    public Tests(ISolution solution)
+    {
+        _solution = solution;
+    }
+
+    [Test]
+    public void Example1()
+    {
+        Assert.That(_solution.IsPalindrome(121), Is.True);
+    }
+
+    [Test]
+    public void Example2()
+    {
+        Assert.That(_solution.IsPalindrome(-121), Is.False);
+    }
+
+    [Test]
+    public void Example3()
+    {
+        Assert.That(_solution.IsPalindrome(10), Is.False);
+    }
+
+    private static IEnumerable<ISolution> Solutions
+    {
+        get
+        {
+            yield return new Solution();
+        }
+    }
+}
---
+++ LeetCode/LeetCode/009 Palindrome Number/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._009_Palindrome_Number;
+
+public interface ISolution
+{
+    public bool IsPalindrome(int x);
+}

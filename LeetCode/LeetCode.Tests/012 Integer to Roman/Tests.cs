63cd0fc1980e029dcd4d9d669ddb92b78ee9b3b8
Michael Naumov <mnaoumov@gmail.com>
2022-09-25 12:00:09 -0600

    012

---
+++ LeetCode/LeetCode.Tests/012 Integer to Roman/Tests.cs
@@ -0,0 +1,41 @@
+﻿using LeetCode._012_Integer_to_Roman;
+using NUnit.Framework;
+
+namespace LeetCode.Tests._012_Integer_to_Roman;
+
+[TestFixtureSource(nameof(Solutions))]
+public class Tests
+{
+    private readonly ISolution _solution;
+
+    public Tests(ISolution solution)
+    {
+        _solution = solution;
+    }
+
+    [Test]
+    public void Example1()
+    {
+        Assert.That(_solution.IntToRoman(3), Is.EqualTo("III"));
+    }
+
+    [Test]
+    public void Example2()
+    {
+        Assert.That(_solution.IntToRoman(58), Is.EqualTo("LVIII"));
+    }
+
+    [Test]
+    public void Example3()
+    {
+        Assert.That(_solution.IntToRoman(1994), Is.EqualTo("MCMXCIV"));
+    }
+
+    private static IEnumerable<ISolution> Solutions
+    {
+        get
+        {
+            yield return new Solution();
+        }
+    }
+}
---
+++ LeetCode/LeetCode/012 Integer to Roman/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._012_Integer_to_Roman;
+
+public interface ISolution
+{
+    public string IntToRoman(int num);
+}

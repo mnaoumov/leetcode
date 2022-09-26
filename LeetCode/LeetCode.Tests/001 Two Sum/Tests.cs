e9cd0bae914befcc0698c2b9a7b0dcec77aead0e
Michael Naumov <mnaoumov@gmail.com>
2022-09-26 15:54:07 -0600

    Extract TestsBase

--- LeetCode/LeetCode.Tests/001 Two Sum/Tests.cs
+++ LeetCode/LeetCode.Tests/001 Two Sum/Tests.cs
@@ -4,48 +4,30 @@ using NUnit.Framework;
 namespace LeetCode.Tests._001_Two_Sum;
 
 [TestFixtureSource(nameof(Solutions))]
-public class Tests
+public class Tests : TestsBase<ISolution>
 {
-    private readonly ISolution _solution;
-
-    public Tests(ISolution solution)
+    public Tests(ISolution solution) : base(solution)
     {
-        _solution = solution;
     }
 
     [Test]
     public void Example1()
     {
-        var result = _solution.TwoSum(new[] { 2, 7, 11, 15 }, 9);
+        var result = Solution.TwoSum(new[] { 2, 7, 11, 15 }, 9);
         Assert.That(result, Is.EqualTo(new[] { 0, 1 }));
     }
 
     [Test]
     public void Example2()
     {
-        var result = _solution.TwoSum(new[] { 3, 2, 4 }, 6);
+        var result = Solution.TwoSum(new[] { 3, 2, 4 }, 6);
         Assert.That(result, Is.EqualTo(new[] { 1, 2 }));
     }
 
     [Test]
     public void Example3()
     {
-        var result = _solution.TwoSum(new[] { 3, 3 }, 6);
+        var result = Solution.TwoSum(new[] { 3, 3 }, 6);
         Assert.That(result, Is.EqualTo(new[] { 0, 1 }));
-    }
-
-    private static IEnumerable<ISolution> Solutions
-    {
-        get
-        {
-            yield return new BruteForceSolution();
-            yield return new LookupSolution();
-            yield return new HashMapSolution();
-            yield return new HashMapOnePassSolution();
-            yield return new OldSolution1();
-            yield return new OldSolution2();
-            yield return new OldSolution3();
-            yield return new OldSolution4();
-        }
     }
 }
---
+++ LeetCode/LeetCode.Tests/TestsBase.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Tests;
+
+public abstract class TestsBase<TSolution>
+{
+    protected TSolution Solution { get; }
+
+    protected TestsBase(TSolution solution)
+    {
+        Solution = solution;
+    }
+
+    protected static IEnumerable<TSolution> Solutions
+    {
+        get
+        {
+            var solutionInterfaceType = typeof(TSolution);
+            var solutionTypes = solutionInterfaceType.Assembly.GetTypes().Where(t => t.IsClass && t.IsAssignableTo(solutionInterfaceType));
+            var solutions = solutionTypes.Select(t => (TSolution) Activator.CreateInstance(t)!);
+            return solutions;
+        }
+    }
+}

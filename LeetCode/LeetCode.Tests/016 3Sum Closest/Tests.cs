e9c84d543bc8109b97974561733c4252dcc7309c
Michael Naumov <mnaoumov@gmail.com>
2022-09-26 13:54:37 -0600

    016 TLE

---
+++ LeetCode/LeetCode.Tests/016 3Sum Closest/Tests.cs
@@ -0,0 +1,35 @@
+﻿using LeetCode._016_3Sum_Closest;
+using NUnit.Framework;
+
+namespace LeetCode.Tests._016_3Sum_Closest;
+
+[TestFixtureSource(nameof(Solutions))]
+public class Tests
+{
+    private readonly ISolution _solution;
+
+    public Tests(ISolution solution)
+    {
+        _solution = solution;
+    }
+
+    [Test]
+    public void Example1()
+    {
+        Assert.That(_solution.ThreeSumClosest(new [] { -1, 2, 1, -4 }, 1), Is.EqualTo(2));
+    }
+
+    [Test]
+    public void Example2()
+    {
+        Assert.That(_solution.ThreeSumClosest(new [] { 0, 0, 0 }, 1), Is.EqualTo(0));
+    }
+
+    private static IEnumerable<ISolution> Solutions
+    {
+        get
+        {
+            yield return new Solution();
+        }
+    }
+}
---
+++ LeetCode/LeetCode/016 3Sum Closest/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._016_3Sum_Closest;
+
+public interface ISolution
+{
+    public int ThreeSumClosest(int[] nums, int target);
+}

e48074580d59b6579f4d03c2075472bfd4a2e690
Michael Naumov <mnaoumov@gmail.com>
2022-09-26 21:57:32 -0600

    017

---
+++ LeetCode/LeetCode.Tests/017 Letter Combinations of a Phone Number/Tests.cs
@@ -0,0 +1,30 @@
+﻿using LeetCode._017_Letter_Combinations_of_a_Phone_Number;
+using NUnit.Framework;
+
+namespace LeetCode.Tests._017_Letter_Combinations_of_a_Phone_Number;
+
+[TestFixtureSource(nameof(Solutions))]
+public class Tests : TestsBase<ISolution>
+{
+    public Tests(ISolution solution) : base(solution)
+    {
+    }
+
+    [Test]
+    public void Example1()
+    {
+        Assert.That(Solution.LetterCombinations("23"), Is.EquivalentTo(new[] { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" }));
+    }
+
+    [Test]
+    public void Example2()
+    {
+        Assert.That(Solution.LetterCombinations(""), Is.EquivalentTo(Array.Empty<string>()));
+    }
+
+    [Test]
+    public void Example3()
+    {
+        Assert.That(Solution.LetterCombinations("2"), Is.EquivalentTo(new[] { "a", "b", "c" }));
+    }
+}
---
+++ LeetCode/LeetCode/017 Letter Combinations of a Phone Number/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._017_Letter_Combinations_of_a_Phone_Number;
+
+public interface ISolution
+{
+    public IList<string> LetterCombinations(string digits);
+}

4a342eed129f7437580480cd6670c54fe6d27cdd
Michael Naumov <mnaoumov@gmail.com>
2022-09-27 18:03:05 -0600

    Write tests

--- LeetCode/LeetCode/018 4Sum/Tests.cs
+++ LeetCode/LeetCode/018 4Sum/Tests.cs
@@ -5,15 +5,23 @@ using NUnit.Framework;
 namespace LeetCode._018_4Sum;
 
 [TestFixtureSource(nameof(Solutions))]
-public class Tests : TestsBase<_Template.ISolution>
+public class Tests : TestsBase<ISolution>
 {
-    public Tests(_Template.ISolution solution) : base(solution)
+    public Tests(ISolution solution) : base(solution)
     {
     }
 
     [Test]
     public void Example1()
     {
-        Assert.That(Solution, Is.Not.Null);
+        Assert.That(Solution.FourSum(new[] { 1, 0, -1, 0, -2, 2 }, 0),
+            IsEquivalentToIgnoringItemOrder(new[] { new[] { -2, -1, 1, 2 }, new[] { -2, 0, 0, 2 }, new[] { -1, 0, 0, 1 } }));
+    }
+
+    [Test]
+    public void Example2()
+    {
+        Assert.That(Solution.FourSum(new[] { 2, 2, 2, 2, 2 }, 8),
+            IsEquivalentToIgnoringItemOrder(new[] { new[] { -2, 2, 2, 2 } }));
     }
 }

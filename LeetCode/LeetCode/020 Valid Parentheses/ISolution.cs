ca95338bb1e940752c69346fa7d2c41f9a60d97e
Michael Naumov <mnaoumov@gmail.com>
2022-09-27 22:10:15 -0600

    020

---
+++ LeetCode/LeetCode/020 Valid Parentheses/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._020_Valid_Parentheses;
+
+public interface ISolution
+{
+    public bool IsValid(string s);
+}
---
+++ LeetCode/LeetCode/020 Valid Parentheses/Tests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._020_Valid_Parentheses;
+
+[TestFixtureSource(nameof(Solutions))]
+public class Tests : TestsBase<ISolution>
+{
+    public Tests(ISolution solution) : base(solution)
+    {
+    }
+
+    [Test]
+    public void Example1()
+    {
+        Assert.That(Solution.IsValid("()"), Is.True);
+    }
+
+    [Test]
+    public void Example2()
+    {
+        Assert.That(Solution.IsValid("()[]{}"), Is.True);
+    }
+
+    [Test]
+    public void Example3()
+    {
+        Assert.That(Solution.IsValid("(]"), Is.False);
+    }
+}

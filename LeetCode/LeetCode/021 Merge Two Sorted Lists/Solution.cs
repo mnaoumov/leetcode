e5e733775156961380363244c04cdd008cc961a2
Michael Naumov <mnaoumov@gmail.com>
2022-09-28 12:48:13 -0600

    021

---
+++ LeetCode/LeetCode/021 Merge Two Sorted Lists/Solution.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCode._021_Merge_Two_Sorted_Lists;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/810706460/
+/// </summary>
+public class Solution : ISolution
+{
+    public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+    {
+        ListNode? node1 = list1;
+        ListNode? node2 = list2;
+
+        var fakeResultParentNode = new ListNode();
+        var resultNode = fakeResultParentNode;
+
+        while (node1 != null || node2 != null)
+        {
+            ref var minNode = ref node1;
+
+            if (node1 != null)
+            {
+                if (node2 != null)
+                {
+                    if (node1.val <= node2.val)
+                    {
+                        minNode = ref node1;
+                    }
+                    else
+                    {
+                        minNode = ref node2;
+                    }
+                }
+                else
+                {
+                    minNode = ref node1;
+                }
+            }
+            else
+            {
+                minNode = ref node2;
+            }
+
+            resultNode.next = new ListNode(minNode!.val);
+            resultNode = resultNode.next;
+            minNode = minNode.next;
+        }
+
+        return fakeResultParentNode.next!;
+    }
+}
---
+++ LeetCode/LeetCode/021 Merge Two Sorted Lists/Tests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._021_Merge_Two_Sorted_Lists;
+
+[TestFixtureSource(nameof(Solutions))]
+public class Tests : TestsBase<ISolution>
+{
+    public Tests(ISolution solution) : base(solution)
+    {
+    }
+
+    [Test]
+    public void Example1()
+    {
+        Assert.That(Solution.MergeTwoLists(ListNode.Create(1, 2, 4), ListNode.Create(1, 3, 4)), Is.EqualTo(ListNode.Create(1, 1, 2, 3, 4, 4)));
+    }
+}

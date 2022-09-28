96aa80b9e8706c512060353c66fcb8e3dd806d9b
Michael Naumov <mnaoumov@gmail.com>
2022-09-28 13:32:38 -0600

    022

---
+++ LeetCode/LeetCode/022 Generate Parentheses/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._022_Generate_Parentheses;
+
+public interface ISolution
+{
+    IList<string> GenerateParenthesis(int n);
+}
---
+++ LeetCode/LeetCode/022 Generate Parentheses/Tests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+namespace LeetCode._022_Generate_Parentheses;
+
+[TestFixtureSource(nameof(Solutions))]
+public class Tests : TestsBase<ISolution>
+{
+    public Tests(ISolution solution) : base(solution)
+    {
+    }
+
+    [Test]
+    public void Example1()
+    {
+        Assert.That(Solution.GenerateParenthesis(3),
+            Is.EquivalentTo(new[] { "((()))", "(()())", "(())()", "()(())", "()()()" }));
+    }
+    
+    [Test]
+    public void Example2()
+    {
+        Assert.That(Solution.GenerateParenthesis(1),
+            Is.EquivalentTo(new[] { "()" }));
+    }
+}

1aec72e88785ec906c91c02931bfcbe5c64d386a
Michael Naumov <mnaoumov@gmail.com>
2022-09-29 11:14:26 -0600

    658

---
+++ LeetCode/LeetCode/658 Find K Closest Elements/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._658_Find_K_Closest_Elements;
+
+public interface ISolution
+{
+    public IList<int> FindClosestElements(int[] arr, int k, int x);
+}
---
+++ LeetCode/LeetCode/658 Find K Closest Elements/Tests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._658_Find_K_Closest_Elements;
+
+[TestFixtureSource(nameof(Solutions))]
+public class Tests : TestsBase<ISolution>
+{
+    public Tests(ISolution solution) : base(solution)
+    {
+    }
+
+    [Test]
+    public void Example1()
+    {
+        Assert.That(Solution.FindClosestElements(new[] { 1, 2, 3, 4, 5 }, 4, 3), Is.EqualTo(new[] { 1, 2, 3, 4 }));
+    }
+
+    [Test]
+    public void Example2()
+    {
+        Assert.That(Solution.FindClosestElements(new[] { 1, 2, 3, 4, 5 }, 4, -1), Is.EqualTo(new[] { 1, 2, 3, 4 }));
+    }
+}

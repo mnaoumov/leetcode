d50e954351f9854e150d7421e5f04ca4f9e8402a
Michael Naumov <mnaoumov@gmail.com>
2022-09-28 23:10:48 -0600

    024

---
+++ LeetCode/LeetCode/024 Swap Nodes in Pairs/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._024_Swap_Nodes_in_Pairs;
+
+public interface ISolution
+{
+    public ListNode? SwapPairs(ListNode? head);
+}
---
+++ LeetCode/LeetCode/024 Swap Nodes in Pairs/Tests.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._024_Swap_Nodes_in_Pairs;
+
+[TestFixtureSource(nameof(Solutions))]
+public class Tests : TestsBase<ISolution>
+{
+    public Tests(ISolution solution) : base(solution)
+    {
+    }
+
+    [Test]
+    public void Example1()
+    {
+        TestSwapPairs(valuesBeforeSwapping: new[] { 1, 2, 3, 4 }, valuesAfterSwapping: new[] { 2, 1, 4, 3 });
+    }
+
+    [Test]
+    public void Example2()
+    {
+        TestSwapPairs(valuesBeforeSwapping: Array.Empty<int>(), valuesAfterSwapping: Array.Empty<int>());
+    }
+
+    [Test]
+    public void Example3()
+    {
+        TestSwapPairs(valuesBeforeSwapping: new[] { 1 }, valuesAfterSwapping: new[] { 1 });
+    }
+
+    private void TestSwapPairs(int[] valuesBeforeSwapping, int[] valuesAfterSwapping)
+    {
+        var listBeforeSwapping = valuesBeforeSwapping.Any() ? ListNode.Create(valuesBeforeSwapping) : null;
+
+        var valueToNodeMap = new Dictionary<int, ListNode>();
+
+        var node = listBeforeSwapping;
+
+        while (node != null)
+        {
+            valueToNodeMap.Add(node.val, node);
+            node = node.next;
+        }
+
+        var listAfterSwapping = Solution.SwapPairs(listBeforeSwapping);
+
+        node = listAfterSwapping;
+
+        foreach (var valueAfterSwapping in valuesAfterSwapping)
+        {
+            Assert.That(node, Is.SameAs(valueToNodeMap[valueAfterSwapping]));
+            node = node!.next;
+        }
+
+        Assert.That(node, Is.Null);
+    }
+}

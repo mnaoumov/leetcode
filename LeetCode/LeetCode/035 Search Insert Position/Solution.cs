130cace81fc12ee114131d97128cf25bc856491f
Michael Naumov <mnaoumov@gmail.com>
2022-10-01 15:43:09 -0600

    035

---
+++ LeetCode/LeetCode/035 Search Insert Position/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCode._035_Search_Insert_Position;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/813031657/
+/// </summary>
+public class Solution : ISolution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left <= right)
+        {
+            var mid = (left + right) / 2;
+
+            if (nums[mid] < target)
+            {
+                left = mid + 1;
+            }
+            else if (nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+
+        return left;
+    }
+}

01d4650a4bbb6f71d33bcb8ab52c8abfdfd1fade
Michael Naumov <mnaoumov@gmail.com>
2022-09-30 18:57:28 -0600

    091

---
+++ LeetCode/LeetCode/091 Decode Ways/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._091_Decode_Ways;
+
+public interface ISolution
+{
+    public int NumDecodings(string s);
+}
---
+++ LeetCode/LeetCode/091 Decode Ways/Solution.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCode._091_Decode_Ways;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/812369551/
+/// </summary>
+public class Solution : ISolution
+{
+    public int NumDecodings(string s)
+    {
+        var cache = new int?[s.Length];
+        return NumDecodings(0);
+
+        int NumDecodings(int index)
+        {
+            if (index == s.Length)
+            {
+                return 1;
+            }
+
+            if (index > s.Length)
+            {
+                return 0;
+            }
+
+            if (cache[index] is { } result)
+            {
+                return result;
+            }
+            else
+            {
+                return (int) (cache[index] = CalculateNumDecodings(index));
+            }
+        }
+
+        int CalculateNumDecodings(int index)
+        {
+            var digit = GetDigit(index);
+
+            if (digit == 0)
+            {
+                return 0;
+            }
+
+            var result = NumDecodings(index + 1);
+
+            switch (digit)
+            {
+                case 1:
+                case 2 when index + 1 < s.Length && GetDigit(index + 1) <= 6:
+                    result += NumDecodings(index + 2);
+                    break;
+            }
+
+            return result;
+        }
+
+        int GetDigit(int index)
+        {
+            return s[index] - '0';
+        }
+    }
+}

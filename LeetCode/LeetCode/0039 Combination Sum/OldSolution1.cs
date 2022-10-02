2bd9605d21ef3bab24b65f8aabc53afc6e413373
Michael Naumov <mnaoumov@gmail.com>
2022-10-02 17:50:37 -0600

    0039 Old solutions

---
+++ LeetCode/LeetCode/0039 Combination Sum/OldSolution1.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode._0039_Combination_Sum;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/193999858/
+/// https://leetcode.com/submissions/detail/195069394/
+/// </summary>
+public class OldSolution1 : ISolution
+{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+        if (target < 0)
+            return new IList<int>[0];
+
+        if (target == 0)
+            return new IList<int>[] { new int[0] };
+
+        List<IList<int>> result = new List<IList<int>>();
+        foreach (var candidate in candidates)
+        {
+            var candidatesStartingFromCurrent = candidates.SkipWhile(c => c != candidate).ToArray();
+            var targetAfterTakingCurrentCandidate = target - candidate;
+            var resultsAfterTakingCurrentCandidate = CombinationSum(candidatesStartingFromCurrent, targetAfterTakingCurrentCandidate);
+            result.AddRange(resultsAfterTakingCurrentCandidate.Select(newResult => new[] { candidate }.Concat(newResult).ToArray()));
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/LeetCode/0039 Combination Sum/OldSolution2.cs
@@ -0,0 +1,41 @@
+﻿namespace LeetCode._0039_Combination_Sum;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/195822588/
+/// </summary>
+public class OldSolution2 : ISolution
+{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target)
+    {
+        return CombinationSum(candidates, target, 0);
+    }
+
+    private IList<IList<int>> CombinationSum(int[] candidates, int target, int startCandidateIndex)
+    {
+        if (target < 0)
+        {
+            return new List<IList<int>>();
+        }
+
+        if (target == 0)
+        {
+            return new List<IList<int>> { new List<int>() };
+        }
+
+        var combinationSums = new List<IList<int>>();
+
+        for (int candidateIndex = startCandidateIndex; candidateIndex < candidates.Length; candidateIndex++)
+        {
+            var candidate = candidates[candidateIndex];
+            var subTaskTarget = target - candidate;
+            var subTaskCombinationSums = CombinationSum(candidates, subTaskTarget, candidateIndex);
+            foreach (var subTaskCombinationSum in subTaskCombinationSums)
+            {
+                subTaskCombinationSum.Insert(0, candidate);
+                combinationSums.Add(subTaskCombinationSum);
+            }
+        }
+
+        return combinationSums;
+    }
+}

eaf21cf13428823e49701fdac8e11f5fc30ab458
Michael Naumov <mnaoumov@gmail.com>
2022-10-03 03:04:08 -0600

    0040

--- LeetCode/LeetCode/0040 Combination Sum II/Solution.cs
+++ LeetCode/LeetCode/0040 Combination Sum II/Solution.cs
@@ -1,13 +1,21 @@
 ﻿namespace LeetCode._0040_Combination_Sum_II;
 
+/// <summary>
+/// https://leetcode.com/submissions/detail/814099332/
+/// </summary>
 public class Solution : ISolution
 {
     public IList<IList<int>> CombinationSum2(int[] candidates, int target)
     {
-        return CombinationSum(candidates, target, 0).ToArray();
+        var candidatesWithCounts = candidates
+            .OrderBy(x => x)
+            .GroupBy(x => x)
+            .Select(g => (value: g.Key, count: g.Count()))
+            .ToArray();
+        return CombinationSum(candidatesWithCounts, target, 0).ToArray();
     }
 
-    private static IEnumerable<IList<int>> CombinationSum(IReadOnlyList<int> candidates, int target, int index)
+    private static IEnumerable<IList<int>> CombinationSum((int value, int count)[] candidatesWithCounts, int target, int index)
     {
         if (target == 0)
         {
@@ -15,32 +23,25 @@ public class Solution : ISolution
             yield break;
         }
 
-        if (target < 0 || index >= candidates.Count)
+        if (target < 0 || index >= candidatesWithCounts.Length)
         {
             yield break;
         }
 
-        if (candidates.Skip(index).Sum() < target)
+        if (candidatesWithCounts.Skip(index).Select(p => p.value * p.count).Sum() < target)
         {
             yield break;
         }
 
-        var keys = new HashSet<string>();
-
-
-        for (var i = index; i < candidates.Count; i++)
+        for (var i = index; i < candidatesWithCounts.Length; i++)
         {
-            var candidate = candidates[i];
+            var (candidate, count) = candidatesWithCounts[i];
 
-            foreach (var combinationRest in CombinationSum(candidates, target - candidate, i + 1))
+            for (var j = 1; j <= count; j++)
             {
-                var combination = new[] { candidate }.Concat(combinationRest).OrderBy(x => x).ToArray();
-
-                var key = string.Join(",", combination);
-
-                if (keys.Add(key))
+                foreach (var combinationRest in CombinationSum(candidatesWithCounts, target - candidate * j, i + 1))
                 {
-                    yield return combination;
+                    yield return Enumerable.Repeat(candidate, j).Concat(combinationRest).OrderBy(x => x).ToArray();
                 }
             }
         }

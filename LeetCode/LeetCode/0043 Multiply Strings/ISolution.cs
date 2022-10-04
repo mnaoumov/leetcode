1fcef1bbdf3f95385eb0228f0fe7b675c742a0d3
Michael Naumov <mnaoumov@gmail.com>
2022-10-03 21:27:47 -0600

    0043

---
+++ LeetCode/LeetCode/0043 Multiply Strings/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._0043_Multiply_Strings;
+
+public interface ISolution
+{
+    public string Multiply(string num1, string num2);
+}
---
+++ LeetCode/LeetCode/0043 Multiply Strings/Tests.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._0043_Multiply_Strings;
+
+public class Tests : TestsBase<ISolution, Tests.TestCase>
+{
+    protected override void TestImpl(ISolution solution, TestCase testCase)
+    {
+        Assert.That(solution.Multiply(testCase.Num1, testCase.Num2), Is.EqualTo(testCase.Return));
+    }
+
+    public class TestCase : TestCaseBase<TestCase>
+    {
+        public string Num1 { get; private init; } = null!;
+        public string Num2 { get; private init; } = null!;
+        public string Return { get; private init; } = null!;
+
+        public override IEnumerable<TestCase> TestCases
+        {
+            get
+            {
+                yield return new TestCase
+                {
+                    Num1 = "2",
+                    Num2 = "3",
+                    Return = "6",
+                    TestCaseName = "Example 1"
+                };
+
+                yield return new TestCase
+                {
+                    Num1 = "123",
+                    Num2 = "456",
+                    Return = "56088",
+                    TestCaseName = "Example 2"
+                };
+            }
+        }
+    }
+}

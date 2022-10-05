51bb9f154abcdaa97229e7e904d8f0ceb410499f
Michael Naumov <mnaoumov@gmail.com>
2022-10-04 23:49:40 -0600

    Add 044 Old solutions

---
+++ LeetCode/LeetCode/0044 Wildcard Matching/OldSolution1LTE.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCode._0044_Wildcard_Matching;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/208757970/
+/// </summary>
+public class OldSolution1LTE : ISolution
+{
+    public bool IsMatch(string s, string p)
+    {
+        return IsMatch(s, p, 0, 0);
+    }
+
+    private bool IsMatch(string s, string p, int sIndex, int pIndex)
+    {
+        const char singleWildcardSymbol = '?';
+        const char multipleWildcardSymbol = '*';
+
+        if (pIndex == p.Length)
+        {
+            return sIndex == s.Length;
+        }
+        var patternSymbol = p[pIndex];
+
+        if (patternSymbol == multipleWildcardSymbol)
+        {
+            for (int afterWildcardIndex = sIndex; afterWildcardIndex <= s.Length; afterWildcardIndex++)
+            {
+                if (IsMatch(s, p, afterWildcardIndex, pIndex + 1))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        else
+        {
+            return sIndex < s.Length && (s[sIndex] == patternSymbol || patternSymbol == singleWildcardSymbol) &&
+                   IsMatch(s, p, sIndex + 1, pIndex + 1);
+        }
+    }
+}
---
+++ LeetCode/LeetCode/0044 Wildcard Matching/OldSolution2LTE.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode._0044_Wildcard_Matching;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/208759054/
+/// </summary>
+public class OldSolution2LTE : ISolution
+{
+    public bool IsMatch(string s, string p)
+    {
+        return IsMatch(s, p, 0, 0);
+    }
+
+    private bool IsMatch(string s, string p, int sIndex, int pIndex)
+    {
+        const char singleWildcardSymbol = '?';
+        const char multipleWildcardSymbol = '*';
+
+        if (pIndex == p.Length)
+        {
+            return sIndex == s.Length;
+        }
+        var patternSymbol = p[pIndex];
+
+        if (patternSymbol == multipleWildcardSymbol)
+        {
+            while (pIndex + 1 < p.Length && p[pIndex + 1] == multipleWildcardSymbol)
+            {
+                pIndex++;
+            }
+
+            for (int afterWildcardIndex = sIndex; afterWildcardIndex <= s.Length; afterWildcardIndex++)
+            {
+                if (IsMatch(s, p, afterWildcardIndex, pIndex + 1))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        else
+        {
+            return sIndex < s.Length && (s[sIndex] == patternSymbol || patternSymbol == singleWildcardSymbol) &&
+                   IsMatch(s, p, sIndex + 1, pIndex + 1);
+        }
+    }
+}

4a5b1f581d9719b36b3b622388c0f1d3bfea0a8d
Michael Naumov <mnaoumov@gmail.com>
2022-10-06 21:36:41 -0600

    Failing test

--- LeetCode/LeetCode/0732 My Calendar III/Tests.cs
+++ LeetCode/LeetCode/0732 My Calendar III/Tests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using LeetCode._0002_Add_Two_Numbers;
+using NUnit.Framework;
 
 namespace LeetCode._0732_My_Calendar_III;
 
@@ -30,6 +31,24 @@ public class Tests : TestsBase<ISolution, Tests.TestCase>
                         Assert.That(sut.Book(25, 55), Is.EqualTo(3));
                     },
                     TestCaseName = "Example 1"
+                };
+
+                yield return new TestCase
+                {
+                    Test = sut =>
+                    {
+                        Assert.That(sut.Book(26, 35), Is.EqualTo(1));
+                        Assert.That(sut.Book(26, 32), Is.EqualTo(2));
+                        Assert.That(sut.Book(25, 32), Is.EqualTo(3));
+                        Assert.That(sut.Book(18, 26), Is.EqualTo(3));
+                        Assert.That(sut.Book(40, 45), Is.EqualTo(3));
+                        Assert.That(sut.Book(19, 26), Is.EqualTo(3));
+                        Assert.That(sut.Book(48, 50), Is.EqualTo(3));
+                        Assert.That(sut.Book(1, 6), Is.EqualTo(3));
+                        Assert.That(sut.Book(46, 50), Is.EqualTo(3));
+                        Assert.That(sut.Book(11, 18), Is.EqualTo(3));
+                    },
+                    TestCaseName = nameof(Solution1Wrong)
                 };
             }
         }

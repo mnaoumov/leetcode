da3491cf0de25e78b6819d5a5fb85c354079ecf5
Michael Naumov <mnaoumov@gmail.com>
2022-10-10 09:09:55 -0600

    55

---
+++ LeetCode/0055 Jump Game/Solution2.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode._0055_Jump_Game;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/819419839/
+/// </summary>
+public class Solution2 : ISolution
+{
+    public bool CanJump(int[] nums)
+    {
+        var cache = new bool?[nums.Length];
+
+        return GetCachedOrCalculate(0);
+
+        bool GetCachedOrCalculate(int index)
+        {
+            if (cache[index] is not { } result)
+            {
+                cache[index] = result = Calculate();
+            }
+
+            return result;
+
+            bool Calculate()
+            {
+                if (index == nums.Length - 1)
+                {
+                    return true;
+                }
+
+                var maxJump = Math.Min(nums[index], nums.Length - 1 - index);
+
+                for (var i = 1; i <= maxJump; i++)
+                {
+                    if (GetCachedOrCalculate(index + i))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+    }
+}

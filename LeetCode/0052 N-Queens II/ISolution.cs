b8757724a002a9826a91ec970ad29d760abd4d58
Michael Naumov <mnaoumov@gmail.com>
2022-10-09 22:20:04 -0600

    0052

---
+++ LeetCode/0052 N-Queens II/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._0052_N_Queens_II;
+
+public interface ISolution
+{
+    public int TotalNQueens(int n);
+}
---
+++ LeetCode/0052 N-Queens II/Tests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._0052_N_Queens_II;
+
+public class Tests : TestsBase<ISolution, Tests.TestCase>
+{
+    protected override void TestImpl(ISolution solution, TestCase testCase)
+    {
+        Assert.That(solution.TotalNQueens(testCase.N), Is.EqualTo(testCase.Output));
+    }
+
+    public class TestCase : TestCaseBase<TestCase>
+    {
+        public int N { get; private init; }
+        public int Output { get; private init; }
+
+        public override IEnumerable<TestCase> TestCases
+        {
+            get
+            {
+                yield return new TestCase
+                {
+                    N = 4,
+                    Output = 2,
+                    TestCaseName = "Example 1"
+                };
+
+                yield return new TestCase
+                {
+                    N = 1,
+                    Output = 1,
+                    TestCaseName = "Example 2"
+                };
+            }
+        }
+    }
+}

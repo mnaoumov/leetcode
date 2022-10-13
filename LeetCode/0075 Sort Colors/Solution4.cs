1aa7f918c6ac5a5243b74304d397d6454f7baf2a
Michael Naumov <mnaoumov@gmail.com>
2022-10-12 18:12:36 -0600

    75

---
+++ LeetCode/0075 Sort Colors/Solution4.cs
@@ -0,0 +1,46 @@
+﻿// ReSharper disable All
+namespace LeetCode._0075_Sort_Colors;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/821249541/
+/// </summary>
+public class Solution4 : ISolution
+{
+    public void SortColors(int[] nums)
+    {
+        var count0 = 0;
+        var count1 = 0;
+        var count2 = 0;
+
+        foreach (var num in nums)
+        {
+            switch (num)
+            {
+                case 0:
+                    count0++;
+                    break;
+                case 1:
+                    count1++;
+                    break;
+                case 2:
+                    count2++;
+                    break;
+            }
+        }
+
+        for (var i = 0; i < count0; i++)
+        {
+            nums[i] = 0;
+        }
+
+        for (var i = count0; i < count0 + count1; i++)
+        {
+            nums[i] = 1;
+        }
+
+        for (var i = count0 + count1; i < nums.Length; i++)
+        {
+            nums[i] = 2;
+        }
+    }
+}

28803c548f0051507c85e29b32bcff5524c678cf
Michael Naumov <mnaoumov@gmail.com>
2022-10-12 18:23:28 -0600

    237

---
+++ LeetCode/0237 Delete Node in a Linked List/Solution2.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode._0237_Delete_Node_in_a_Linked_List;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/821254721/
+/// </summary>
+public class Solution2 : ISolution
+{
+    public void DeleteNode(ListNode node)
+    {
+        while (true)
+        {
+            node.val = node.next!.val;
+            
+            if (node.next.next == null)
+            {
+                node.next = null;
+                break;
+            }
+
+            node = node.next;
+        }
+    }
+}

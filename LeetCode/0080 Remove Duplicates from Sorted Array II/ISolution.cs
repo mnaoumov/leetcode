6d7574423a1f3812c35c078e218a315de2ef69b4
Michael Naumov <mnaoumov@gmail.com>
2022-10-13 11:00:38 -0600

    80

---
+++ LeetCode/0080 Remove Duplicates from Sorted Array II/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._0080_Remove_Duplicates_from_Sorted_Array_II;
+
+public interface ISolution
+{
+    public int RemoveDuplicates(int[] nums);
+}
---
+++ LeetCode/0080 Remove Duplicates from Sorted Array II/Tests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+
+namespace LeetCode._0080_Remove_Duplicates_from_Sorted_Array_II;
+
+public class Tests : TestsBase<ISolution, Tests.TestCase>
+{
+    protected override void TestImpl(ISolution solution, TestCase testCase)
+    {
+        var k = solution.RemoveDuplicates(testCase.Nums);
+        Assert.That(k, Is.EqualTo(testCase.ExpectedNums.Length));
+        Assert.That(testCase.Nums.Take(k), Is.EqualTo(testCase.ExpectedNums));
+    }
+
+    public class TestCase : TestCaseBase<TestCase>
+    {
+        public int[] Nums { get; private init; } = null!;
+        public int[] ExpectedNums { get; private init; } = null!;
+
+        public override IEnumerable<TestCase> TestCases
+        {
+            get
+            {
+                yield return new TestCase
+                {
+                    Nums = new[] { 1, 1, 1, 2, 2, 3 },
+                    ExpectedNums = new[] { 1, 1, 2, 2, 3 },
+                    TestCaseName = "Example 1"
+                };
+
+                yield return new TestCase
+                {
+                    Nums = new[] { 0, 0, 1, 1, 1, 1, 2, 3, 3 },
+                    ExpectedNums = new[] { 0, 0, 1, 1, 2, 3, 3 },
+                    TestCaseName = "Example 2"
+                };
+            }
+        }
+    }
+}

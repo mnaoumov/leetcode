bae2ec1f07efbf5ec1750ce0d3d9ffebb6ee5036
Michael Naumov <mnaoumov@gmail.com>
2022-10-13 10:02:12 -0600

    79

---
+++ LeetCode/0079 Word Search/Solution3.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCode._0079_Word_Search;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/821721381/
+/// </summary>
+public class Solution3 : ISolution
+{
+    public bool Exist(char[][] board, string word)
+    {
+        var m = board.Length;
+        var n = board[0].Length;
+        var boardLetterTaken = new bool[m, n];
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                if (ExistsAt(i, j, 0))
+                {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+
+        bool ExistsAt(int i, int j, int letterIndex)
+        {
+            if (letterIndex >= word.Length)
+            {
+                return true;
+            }
+
+            if (i < 0 || i >= m || j < 0 || j >= n)
+            {
+                return false;
+            }
+
+            if (board[i][j] != word[letterIndex])
+            {
+                return false;
+            }
+
+            if (boardLetterTaken[i, j])
+            {
+                return false;
+            }
+
+            boardLetterTaken[i, j] = true;
+
+            var result =
+                ExistsAt(i + 1, j, letterIndex + 1)
+                || ExistsAt(i - 1, j, letterIndex + 1)
+                || ExistsAt(i, j + 1, letterIndex + 1)
+                || ExistsAt(i, j - 1, letterIndex + 1);
+
+            boardLetterTaken[i, j] = false;
+
+            return result;
+        }
+    }
+}

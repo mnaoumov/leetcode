a3da483e3fbeddaab678c2fdbfad5cc0fb067823
Michael Naumov <mnaoumov@gmail.com>
2022-10-14 13:25:04 -0600

    83

---
+++ LeetCode/0083 Remove Duplicates from Sorted List/ISolution.cs
@@ -0,0 +1,6 @@
+﻿namespace LeetCode._0083_Remove_Duplicates_from_Sorted_List;
+
+public interface ISolution
+{
+    public ListNode DeleteDuplicates(ListNode? head);
+}
---
+++ LeetCode/0083 Remove Duplicates from Sorted List/Solution1.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode._0083_Remove_Duplicates_from_Sorted_List;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/822541644/
+/// </summary>
+public class Solution1 : ISolution
+{
+    public ListNode? DeleteDuplicates(ListNode? head)
+    {
+        var parent = head;
+        var node = head?.next;
+
+        while (node != null)
+        {
+            if (node.val == parent!.val)
+            {
+                parent.next = node.next;
+            }
+            else
+            {
+                parent = node;
+            }
+
+            node = node.next;
+        }
+
+        return head;
+    }
+}

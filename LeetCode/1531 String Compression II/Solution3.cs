160a9a093a293d3a7a357e03ea1bb023bcab4326
Michael Naumov <mnaoumov@gmail.com>
2022-10-15 18:24:15 -0600

    Failing test for 1531

---
+++ LeetCode/1531 String Compression II/Solution3.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode._1531_String_Compression_II;
+
+/// <summary>
+/// 
+/// </summary>
+public class Solution3 : ISolution
+{
+    public int GetLengthOfOptimalCompression(string s, int k)
+    {
+        throw new NotImplementedException();
+    }
+}

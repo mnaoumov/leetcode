3072771ce910b5aa75b8abfa4e7be1de4d5d1595
Michael Naumov <mnaoumov@gmail.com>
2022-11-14 19:47:46 -0600

    Extract SutTests

--- LeetCode/0146 LRU Cache/Tests.cs
+++ LeetCode/0146 LRU Cache/Tests.cs
@@ -3,17 +3,10 @@
 namespace LeetCode._0146_LRU_Cache;
 
 [UsedImplicitly]
-public class Tests : TestsBase<ISolution, Tests.TestCase>
+public class Tests : TestsBase<ISolution, SutTestCase>
 {
-    protected override void TestImpl(ISolution solution, TestCase testCase)
+    protected override void TestImpl(ISolution solution, SutTestCase testCase)
     {
-        var sut = solution.Create(testCase.Capacity);
-        testCase.Test(sut);
-    }
-
-    public class TestCase : TestCaseBase
-    {
-        public int Capacity { get; [UsedImplicitly] init; }
-        public Action<ILRUCache> Test { get; [UsedImplicitly] init; } = null!;
+        TestSut<ILRUCache>(solution, testCase);
     }
 }
---
+++ LeetCode/SutTestCase.cs
@@ -0,0 +1,10 @@
+﻿using JetBrains.Annotations;
+
+namespace LeetCode;
+
+public class SutTestCase : TestCaseBase
+{
+    public string[] Commands { get; [UsedImplicitly] init; } = null!;
+    public object[][] Parameters { get; [UsedImplicitly] init; } = null!;
+    public object[] Output { get; [UsedImplicitly] init; } = null!;
+}

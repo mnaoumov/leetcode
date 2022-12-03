c508678010c5ee673b5864475ce76dc84853010b
Michael Naumov <mnaoumov@gmail.com>
2022-12-03 11:10:09 -0600

    .

---
+++ LeetCode/0392 Is Subsequence/Solution4.cs
@@ -0,0 +1,45 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0392_Is_Subsequence;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/853962286/
+/// </summary>
+[UsedImplicitly]
+public class Solution4 : ISolution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        var tLetterIndicesMap = t.Select((letter, index) => (letter, index)).GroupBy(p => p.letter, p => p.index)
+            .ToDictionary(g => g.Key, g => g.ToArray());
+
+        const int notFound = -1;
+
+        var tIndex = notFound;
+
+        foreach (var sLetter in s)
+        {
+            if (!tLetterIndicesMap.ContainsKey(sLetter))
+            {
+                return false;
+            }
+
+            var letterIndices = tLetterIndicesMap[sLetter];
+            var nextIndexOfIndices = Array.BinarySearch(letterIndices, tIndex + 1);
+
+            if (nextIndexOfIndices < 0)
+            {
+                nextIndexOfIndices = ~nextIndexOfIndices;
+            }
+
+            if (nextIndexOfIndices >= letterIndices.Length)
+            {
+                return false;
+            }
+
+            tIndex = letterIndices[nextIndexOfIndices];
+        }
+
+        return true;
+    }
+}
---
+++ LeetCode/0392 Is Subsequence/Solution5.cs
@@ -0,0 +1,54 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0392_Is_Subsequence;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/853984635/
+/// </summary>
+[UsedImplicitly]
+public class Solution5 : ISolution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        var dp = new DynamicProgramming<(int sIndex, int tIndex), int>((key, recursiveFunc) =>
+        {
+            var (sIndex, tIndex) = key;
+
+            if (sIndex < 0 || tIndex < 0)
+            {
+                return 0;
+            }
+
+            if (s[sIndex] == t[tIndex])
+            {
+                return recursiveFunc((sIndex - 1, tIndex - 1)) + 1;
+            }
+
+            return Math.Max(recursiveFunc((sIndex - 1, tIndex)), recursiveFunc((sIndex, tIndex - 1)));
+        });
+
+        return dp.GetOrCalculate((s.Length - 1, t.Length - 1)) == s.Length;
+    }
+
+    private class DynamicProgramming<TKey, TValue> where TKey : notnull
+    {
+        private readonly Func<TKey, Func<TKey, TValue>, TValue> _func;
+        private readonly Dictionary<TKey, TValue> _cache = new();
+
+        public DynamicProgramming(Func<TKey, Func<TKey, TValue>, TValue> func)
+        {
+            _func = func;
+        }
+
+        public TValue GetOrCalculate(TKey key)
+        {
+            if (!_cache.TryGetValue(key, out var value))
+            {
+                _cache[key] = value = _func(key, GetOrCalculate);
+            }
+
+            return value;
+        }
+    }
+
+}

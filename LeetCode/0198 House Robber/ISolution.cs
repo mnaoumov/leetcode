2a56bd29164454e7e6b57ae26a85b3cdb236c83d
Michael Naumov <mnaoumov@gmail.com>
2022-12-13 22:53:19 -0600

    .

---
+++ LeetCode/0198 House Robber/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0198_House_Robber;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int Rob(int[] nums);
+}
---
+++ LeetCode/0198 House Robber/Solution1.cs
@@ -0,0 +1,35 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0198_House_Robber;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/859419358/
+/// </summary>
+[UsedImplicitly]
+public class Solution : ISolution
+{
+    public int Rob(int[] nums)
+    {
+        var n = nums.Length;
+        var dp = new int?[n];
+
+        return Get(0);
+
+        int Get(int i)
+        {
+            if (i >= n)
+            {
+                return 0;
+            }
+
+            if (dp[i] is { } result)
+            {
+                return result;
+            }
+
+            var result2 = Math.Max(nums[i] + Get(i + 2), Get(i + 1));
+            dp[i] = result2;
+            return result2;
+        }
+    }
+}

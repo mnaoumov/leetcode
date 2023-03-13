b670fcd78dad3a4ff0e9caf2c60f2e3e2de99170
Michael Naumov <mnaoumov@gmail.com>
2023-03-13 17:51:14 -0600

    .

--- LeetCode/1326 Minimum Number of Taps to Open to Water a Garden/Solution1.cs
+++ LeetCode/1326 Minimum Number of Taps to Open to Water a Garden/Solution1.cs
@@ -3,7 +3,7 @@ using JetBrains.Annotations;
 namespace LeetCode._1326_Minimum_Number_of_Taps_to_Open_to_Water_a_Garden;
 
 /// <summary>
-/// 
+/// https://leetcode.com/submissions/detail/914676452/
 /// </summary>
 [UsedImplicitly]
 public class Solution1 : ISolution

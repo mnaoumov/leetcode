cbca3983d081072071d5d6b66e9725acc892c5f5
Michael Naumov <mnaoumov@gmail.com>
2023-03-14 11:34:27 -0600

    .

--- LeetCode/1855 Maximum Distance Between a Pair of Values/Solution1.cs
+++ LeetCode/1855 Maximum Distance Between a Pair of Values/Solution1.cs
@@ -3,13 +3,44 @@ using JetBrains.Annotations;
 namespace LeetCode._1855_Maximum_Distance_Between_a_Pair_of_Values;
 
 /// <summary>
-/// 
+/// https://leetcode.com/submissions/detail/915162018/
 /// </summary>
 [UsedImplicitly]
 public class Solution1 : ISolution
 {
     public int MaxDistance(int[] nums1, int[] nums2)
     {
-        throw new NotImplementedException();
+        var result = 0;
+
+        for (var i = 0; i < Math.Min(nums1.Length, nums2.Length); i++)
+        {
+            var num1 = nums1[i];
+
+            if (nums2[i] < num1)
+            {
+                continue;
+            }
+
+            var low = i;
+            var high = nums2.Length - 1;
+
+            while (low <= high)
+            {
+                var mid = low + (high - low >> 1);
+
+                if (nums2[mid] >= num1)
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+
+            result = Math.Max(result, low - i - 1);
+        }
+
+        return result;
     }
 }

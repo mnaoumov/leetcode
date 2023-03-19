cce1d1bcdcef90de8fc8a0129c77cdef86c32ca0
Michael Naumov <mnaoumov@gmail.com>
2023-03-19 17:42:55 -0600

    Fix warnings

--- LeetCode/1485 Clone Binary Tree With Random Pointer/Node.cs
+++ LeetCode/1485 Clone Binary Tree With Random Pointer/Node.cs
@@ -2,10 +2,18 @@ namespace LeetCode._1485_Clone_Binary_Tree_With_Random_Pointer;
 
 public class Node
 {
-    public int val;
+    // ReSharper disable once InconsistentNaming
+    public readonly int val;
+
+    // ReSharper disable once InconsistentNaming
     public Node? left;
+
+    // ReSharper disable once InconsistentNaming
     public Node? right;
+
+    // ReSharper disable once InconsistentNaming
     public Node? random;
+
     public Node(int val = 0, Node? left = null, Node? right = null, Node? random = null)
     {
         this.val = val;
--- LeetCode/1485 Clone Binary Tree With Random Pointer/NodeCopy.cs
+++ LeetCode/1485 Clone Binary Tree With Random Pointer/NodeCopy.cs
@@ -2,10 +2,18 @@ namespace LeetCode._1485_Clone_Binary_Tree_With_Random_Pointer;
 
 public class NodeCopy
 {
+    // ReSharper disable once InconsistentNaming
     public int val;
+
+    // ReSharper disable once InconsistentNaming
     public NodeCopy? left;
+
+    // ReSharper disable once InconsistentNaming
     public NodeCopy? right;
+
+    // ReSharper disable once InconsistentNaming
     public NodeCopy? random;
+
     public NodeCopy(int val = 0, NodeCopy? left = null, NodeCopy? right = null, NodeCopy? random = null)
     {
         this.val = val;

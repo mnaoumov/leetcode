3ec8c45b4262335a0831a0e32e22a87b53407566
Michael Naumov <mnaoumov@gmail.com>
2023-03-29 16:13:32 -0600

    .

---
+++ LeetCode/0404 Sum of Left Leaves/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0404_Sum_of_Left_Leaves;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int SumOfLeftLeaves(TreeNode root);
+}
---
+++ LeetCode/0404 Sum of Left Leaves/Solution1.cs
@@ -0,0 +1,34 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0404_Sum_of_Left_Leaves;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/924487201/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int SumOfLeftLeaves(TreeNode root) => Dfs(root, false);
+
+    private static int Dfs(TreeNode node, bool isLeftChild)
+    {
+        if (node.left == null && node.right == null && isLeftChild)
+        {
+            return node.val;
+        }
+
+        var result = 0;
+
+        if (node.left != null)
+        {
+            result += Dfs(node.left, true);
+        }
+
+        if (node.right != null)
+        {
+            result += Dfs(node.right, false);
+        }
+
+        return result;
+    }
+}

2b9ec30012671ddb145deaddc89d1e6eb67c833e
Michael Naumov <mnaoumov@gmail.com>
2023-03-29 15:42:48 -0600

    .

---
+++ LeetCode/0929 Unique Email Addresses/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0929_Unique_Email_Addresses;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int NumUniqueEmails(string[] emails);
+}
---
+++ LeetCode/0929 Unique Email Addresses/Solution1.cs
@@ -0,0 +1,37 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0929_Unique_Email_Addresses;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/924478042/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int NumUniqueEmails(string[] emails)
+    {
+        var set = new HashSet<string>();
+        var result = 0;
+
+        foreach (var email in emails)
+        {
+            var parts = email.Split('@');
+            var localName = parts[0];
+            var domain = parts[1];
+
+            var parts2 = localName.Split('+');
+            localName = parts2[0];
+
+            localName = localName.Replace(".", "");
+
+            var key = $"{localName}@{domain}";
+
+            if (set.Add(key))
+            {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}

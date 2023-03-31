031027886db89474dcf72b3a923bd7571fbf8a61
Michael Naumov <mnaoumov@gmail.com>
2023-03-30 23:32:00 -0600

    1822 Sign of the Product of an Array

---
+++ LeetCode/1822 Sign of the Product of an Array/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._1822_Sign_of_the_Product_of_an_Array;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int ArraySign(int[] nums);
+}
---
+++ LeetCode/1822 Sign of the Product of an Array/Solution1.cs
@@ -0,0 +1,12 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._1822_Sign_of_the_Product_of_an_Array;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/925203393/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int ArraySign(int[] nums) => nums.Aggregate(1, (prod, num) => prod * Math.Sign(num));
+}

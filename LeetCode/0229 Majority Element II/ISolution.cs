92c2ded436da9516a5512416069dfb0957703581
Michael Naumov <mnaoumov@gmail.com>
2023-03-31 15:53:45 -0600

    0229 Majority Element II

---
+++ LeetCode/0229 Majority Element II/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0229_Majority_Element_II;
+
+[PublicAPI]
+public interface ISolution
+{
+    public IList<int> MajorityElement(int[] nums);
+}
---
+++ LeetCode/0229 Majority Element II/Solution1.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0229_Majority_Element_II;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/925591975/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public IList<int> MajorityElement(int[] nums)
+    {
+        var minCount = nums.Length / 3;
+        return nums.GroupBy(num => num).Where(g => g.Count() > minCount).Select(g => g.Key).ToArray();
+    }
+}

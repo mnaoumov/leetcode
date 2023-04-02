fe9216239297451bac234f79b4f9be7069873ef5
Michael Naumov <mnaoumov@gmail.com>
2023-04-01 18:03:02 -0600

    0504 Base 7

---
+++ LeetCode/0504 Base 7/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0504_Base_7;
+
+[PublicAPI]
+public interface ISolution
+{
+    public string ConvertToBase7(int num);
+}
---
+++ LeetCode/0504 Base 7/Solution1.cs
@@ -0,0 +1,32 @@
+using System.Text;
+using JetBrains.Annotations;
+
+namespace LeetCode._0504_Base_7;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/926235280/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public string ConvertToBase7(int num)
+    {
+        switch (num)
+        {
+            case 0:
+                return "0";
+            case < 0:
+                return $"-{ConvertToBase7(-num)}";
+            case > 0:
+                var sb = new StringBuilder();
+
+                while (num > 0)
+                {
+                    sb.Insert(0, num % 7);
+                    num /= 7;
+                }
+
+                return sb.ToString();
+        }
+    }
+}

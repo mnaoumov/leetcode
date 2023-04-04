fe33ccb2ef05b64ae363fbacdee4d47a465f0cb3
Michael Naumov <mnaoumov@gmail.com>
2023-04-03 23:39:04 -0600

    0709 To Lower Case

---
+++ LeetCode/0709 To Lower Case/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0709_To_Lower_Case;
+
+[PublicAPI]
+public interface ISolution
+{
+    public string ToLowerCase(string s);
+}
---
+++ LeetCode/0709 To Lower Case/Solution1.cs
@@ -0,0 +1,12 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0709_To_Lower_Case;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/927650979/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public string ToLowerCase(string s) => s.ToLower();
+}

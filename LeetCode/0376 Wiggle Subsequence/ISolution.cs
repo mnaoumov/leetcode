97d6a2030c87f1e4adaa77870200fc187083aaec
Michael Naumov <mnaoumov@gmail.com>
2023-04-08 09:48:40 -0600

    0376 Wiggle Subsequence

---
+++ LeetCode/0376 Wiggle Subsequence/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0376_Wiggle_Subsequence;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int WiggleMaxLength(int[] nums);
+}
---
+++ LeetCode/0376 Wiggle Subsequence/Solution1.cs
@@ -0,0 +1,32 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0376_Wiggle_Subsequence;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/930015467/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int WiggleMaxLength(int[] nums)
+    {
+        var last = nums[0];
+        var previousSign = 0;
+        var result = 1;
+        foreach (var num in nums)
+        {
+            var sign = Math.Sign(num - last);
+            if (sign != 0)
+            {
+                if (previousSign != sign)
+                {
+                    result++;
+                }
+                previousSign = sign;
+            }
+
+            last = num;
+        }
+        return result;
+    }
+}

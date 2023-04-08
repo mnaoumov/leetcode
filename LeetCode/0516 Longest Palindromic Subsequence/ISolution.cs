9de9d5612813ce6846b65bd536499d475378ffb1
Michael Naumov <mnaoumov@gmail.com>
2023-04-08 09:49:46 -0600

    0516 Longest Palindromic Subsequence

---
+++ LeetCode/0516 Longest Palindromic Subsequence/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0516_Longest_Palindromic_Subsequence;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int LongestPalindromeSubseq(string s);
+}
---
+++ LeetCode/0516 Longest Palindromic Subsequence/Solution1.cs
@@ -0,0 +1,44 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0516_Longest_Palindromic_Subsequence;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/929999265/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int LongestPalindromeSubseq(string s)
+    {
+        var dp = new Dictionary<(int start, int end), int>();
+        var n = s.Length;
+
+        return Dp(0, n - 1);
+
+        int Dp(int start, int end)
+        {
+            if (start > end)
+            {
+                return 0;
+            }
+
+            if (start == end)
+            {
+                return 1;
+            }
+
+            var key = (start, end);
+            if (dp.TryGetValue(key, out var result))
+            {
+                return result;
+            }
+
+            if (s[start] == s[end])
+            {
+                return dp[key] = 2 + Dp(start + 1, end - 1);
+            }
+
+            return dp[key] = Math.Max(Dp(start + 1, end), Dp(start, end - 1));
+        }
+    }
+}

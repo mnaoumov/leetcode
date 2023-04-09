39b1255563a34b931f1cfe21ef978a40ba0bfcad
Michael Naumov <mnaoumov@gmail.com>
2023-04-08 22:28:18 -0600

    2614 Prime In Diagonal

---
+++ LeetCode/2614 Prime In Diagonal/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2614_Prime_In_Diagonal;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int DiagonalPrime(int[][] nums);
+}
---
+++ LeetCode/2614 Prime In Diagonal/Solution1.cs
@@ -0,0 +1,59 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2614_Prime_In_Diagonal;
+
+/// <summary>
+/// https://leetcode.com/contest/weekly-contest-340/submissions/detail/930421326/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int DiagonalPrime(int[][] nums)
+    {
+        var n = nums.Length;
+
+        var result = 0;
+
+        for (var i = 0; i < n; i++)
+        {
+            var num = nums[i][i];
+
+            if (IsPrime(num))
+            {
+                result = Math.Max(result, num);
+            }
+
+            if (i == n - 1 - i)
+            {
+                continue;
+            }
+
+            num = nums[i][n - 1 - i];
+
+            if (IsPrime(num))
+            {
+                result = Math.Max(result, num);
+            }
+        }
+
+        return result;
+    }
+
+    private static bool IsPrime(int num)
+    {
+        if (num == 1)
+        {
+            return false;
+        }
+
+        for (var j = 2; j * j <= num; j++)
+        {
+            if (num % j == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}

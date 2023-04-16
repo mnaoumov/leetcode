e3a0f3abba814cc98461e0a11299a35e3de2a2ec
Michael Naumov <mnaoumov@gmail.com>
2023-04-16 14:21:34 -0600

    0415 Add Strings

---
+++ LeetCode/0415 Add Strings/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0415_Add_Strings;
+
+[PublicAPI]
+public interface ISolution
+{
+    public string AddStrings(string num1, string num2);
+}
---
+++ LeetCode/0415 Add Strings/Solution1.cs
@@ -0,0 +1,32 @@
+using System.Text;
+using JetBrains.Annotations;
+
+namespace LeetCode._0415_Add_Strings;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/934908582/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public string AddStrings(string num1, string num2)
+    {
+        var index1 = num1.Length - 1;
+        var index2 = num2.Length - 1;
+        var sb = new StringBuilder();
+        var carry = false;
+
+        while (index1 >= 0 || index2 >= 0 || carry)
+        {
+            var digit1 = index1 >= 0 ? num1[index1] - '0' : 0;
+            var digit2 = index2 >= 0 ? num2[index2] - '0' : 0;
+            var digitSum = digit1 + digit2 + (carry ? 1 : 0);
+            carry = digitSum >= 10;
+            sb.Insert(0, digitSum - (carry ? 10 : 0));
+            index1--;
+            index2--;
+        }
+
+        return sb.ToString();
+    }
+}

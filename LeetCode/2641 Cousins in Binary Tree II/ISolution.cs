2728901fba3c4f31e3a14454b787d97009b2dc6f
Michael Naumov <mnaoumov@gmail.com>
2023-04-15 18:13:22 -0600

    2641 Cousins in Binary Tree II

---
+++ LeetCode/2641 Cousins in Binary Tree II/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2641_Cousins_in_Binary_Tree_II;
+
+[PublicAPI]
+public interface ISolution
+{
+    public TreeNode ReplaceValueInTree(TreeNode root);
+}
---
+++ LeetCode/2641 Cousins in Binary Tree II/Solution1.cs
@@ -0,0 +1,53 @@
+using System.Numerics;
+using JetBrains.Annotations;
+
+namespace LeetCode._2641_Cousins_in_Binary_Tree_II;
+
+/// <summary>
+/// https://leetcode.com/contest/biweekly-contest-102/submissions/detail/934191182/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public TreeNode ReplaceValueInTree(TreeNode root)
+    {
+        var queue = new Queue<(TreeNode node, BigInteger index)>();
+        queue.Enqueue((root, 0));
+
+        int count;
+        while ((count = queue.Count) > 0)
+        {
+            var groups = new Dictionary<BigInteger, List<TreeNode>>();
+
+            for (var i = 0; i < count; i++)
+            {
+                var (node, index) = queue.Dequeue();
+                groups.TryAdd(index >> 1, new List<TreeNode>());
+                groups[index >> 1].Add(node);
+
+                if (node.left != null)
+                {
+                    queue.Enqueue((node.left, index * 2));
+                }
+
+                if (node.right != null)
+                {
+                    queue.Enqueue((node.right, index * 2 + 1));
+                }
+            }
+
+            var groupSums = groups.ToDictionary(kvp => kvp.Key, kvp => kvp.Value.Sum(n => n.val));
+            var totalSum = groupSums.Values.Sum();
+
+            foreach (var (parent, group) in groups)
+            {
+                foreach (var node in group)
+                {
+                    node.val = totalSum - groupSums[parent];
+                }
+            }
+        }
+
+        return root;
+    }
+}

0def3605dcf83e4c78437d2c21bf8ab242284d89
Michael Naumov <mnaoumov@gmail.com>
2023-04-21 20:17:00 -0600

    Add more templates

--- LeetCode/Templates/DynamicProgramming.cs
+++ LeetCode/Templates/DynamicProgramming.cs
@@ -1,4 +1,9 @@
-﻿// ReSharper disable All
+﻿// ReSharper disable ClassNeverInstantiated.Local
+// ReSharper disable MemberCanBePrivate.Local
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Local
+// ReSharper disable UnusedType.Local
+
 namespace LeetCode.Templates;
 
 public static partial class Template
---
+++ LeetCode/Templates/TopologicalSort.cs
@@ -0,0 +1,65 @@
+﻿// ReSharper disable ClassNeverInstantiated.Local
+// ReSharper disable MemberCanBePrivate.Local
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Local
+// ReSharper disable UnusedType.Local
+
+namespace LeetCode.Templates;
+
+public static partial class Template
+{
+    private class TopologicalSort<T> where T : notnull
+    {
+        private readonly HashSet<T> _seen = new();
+        private readonly HashSet<T> _processing = new();
+        private readonly DirectedGraph<T> _graph;
+        private readonly Stack<T> _order = new();
+
+        public TopologicalSort(DirectedGraph<T> graph)
+        {
+            _graph = graph;
+            IsAcyclic = true;
+
+            foreach (var node in graph.Nodes)
+            {
+                if (_seen.Contains(node))
+                {
+                    continue;
+                }
+
+                if (Dfs(node))
+                {
+                    continue;
+                }
+
+                IsAcyclic = false;
+                break;
+            }
+        }
+
+        private bool Dfs(T node)
+        {
+            if (!_processing.Add(node))
+            {
+                return false;
+            }
+
+            if (!_seen.Add(node))
+            {
+                return true;
+            }
+
+            if (_graph.AdjacentNodes(node).Any(adjacentNode => !Dfs(adjacentNode)))
+            {
+                return false;
+            }
+
+            _order.Push(node);
+            _processing.Remove(node);
+            return true;
+        }
+
+        private bool IsAcyclic { get; }
+        public IEnumerable<T> Order => IsAcyclic ? _order : Enumerable.Empty<T>();
+    }
+}

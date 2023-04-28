3bb7ffe33b7473cae81dca9f29ceb2d401a48ba1
Michael Naumov <mnaoumov@gmail.com>
2023-04-27 20:02:04 -0600

    1514 Path with Maximum Probability

---
+++ LeetCode/1514 Path with Maximum Probability/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._1514_Path_with_Maximum_Probability;
+
+[PublicAPI]
+public interface ISolution
+{
+    public double MaxProbability(int n, int[][] edges, double[] succProb, int start, int end);
+}
---
+++ LeetCode/1514 Path with Maximum Probability/Tests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+using JetBrains.Annotations;
+
+namespace LeetCode._1514_Path_with_Maximum_Probability;
+
+[UsedImplicitly]
+public class Tests : TestsBase<ISolution, Tests.TestCase>
+{
+    protected override void TestImpl(ISolution solution, TestCase testCase)
+    {
+        Assert.That(solution.MaxProbability(testCase.N, testCase.Edges, testCase.SuccProb, testCase.Start, testCase.End), Is.EqualTo(testCase.Output));
+    }
+
+    public class TestCase : TestCaseBase
+    {
+        public int N { get; [UsedImplicitly] init; }
+        public int[][] Edges { get; [UsedImplicitly] init; } = null!;
+        public double[] SuccProb { get; [UsedImplicitly] init; } = null!;
+        public int Start { get; [UsedImplicitly] init; }
+        public int End { get; [UsedImplicitly] init; }
+        public double Output { get; [UsedImplicitly] init; }
+    }
+}

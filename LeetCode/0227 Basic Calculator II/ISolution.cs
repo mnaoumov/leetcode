8adc45e8274d8d3a65cf8bdc4d7ae78029cfdb0d
Michael Naumov <mnaoumov@gmail.com>
2023-04-29 17:19:09 -0600

    0227 Basic Calculator II

---
+++ LeetCode/0227 Basic Calculator II/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0227_Basic_Calculator_II;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int Calculate(string s);
+}
---
+++ LeetCode/0227 Basic Calculator II/Solution1.cs
@@ -0,0 +1,81 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0227_Basic_Calculator_II;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/941805071/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int Calculate(string s)
+    {
+        var numbers = new Stack<long>();
+        var operators = new Stack<char>();
+        numbers.Push(0L);
+
+        foreach (var symbol in s.Append(default))
+        {
+            if (symbol == ' ')
+            {
+                continue;
+            }
+
+            if (char.IsDigit(symbol))
+            {
+                var digit = symbol - '0';
+                numbers.Push(numbers.Pop() * 10 + digit);
+                continue;
+            }
+
+            while (operators.Count > 0)
+            {
+                var previousOperator = operators.Pop();
+                var num2 = numbers.Pop();
+                var num1 = numbers.Pop();
+
+                if (previousOperator == '*')
+                {
+                    numbers.Push(num1 * num2);
+                }
+                else if (previousOperator == '/')
+                {
+                    numbers.Push(num1 / num2);
+                }
+                else
+                {
+                    var hasHigherPrecedence = symbol is '*' or '/';
+
+                    if (!hasHigherPrecedence)
+                    {
+                        if (previousOperator == '+')
+                        {
+                            numbers.Push(num1 + num2);
+                        }
+                        else if (previousOperator == '-')
+                        {
+                            numbers.Push(num1 - num2);
+                        }
+                    }
+                    else
+                    {
+                        numbers.Push(num1);
+                        numbers.Push(num2);
+                        operators.Push(previousOperator);
+                        break;
+                    }
+                }
+            }
+
+            if (symbol == default)
+            {
+                break;
+            }
+
+            operators.Push(symbol);
+            numbers.Push(0);
+        }
+
+        return (int) numbers.Pop();
+    }
+}

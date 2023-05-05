3e45985e9dbbb313b1ea55066410f479bdf0b78b
Michael Naumov <mnaoumov@gmail.com>
2023-05-05 17:59:10 -0600

    0650 2 Keys Keyboard

---
+++ LeetCode/0650 2 Keys Keyboard/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0650_2_Keys_Keyboard;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int MinSteps(int n);
+}
---
+++ LeetCode/0650 2 Keys Keyboard/Solution1.cs
@@ -0,0 +1,72 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0650_2_Keys_Keyboard;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/945220286/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int MinSteps(int n)
+    {
+        const int impossible = int.MaxValue;
+
+        var dp = new DynamicProgramming<(int onScreenCount, int bufferCount), int>((key, recursiveFunc) =>
+        {
+            var (onScreenCount, bufferCount) = key;
+
+            if (onScreenCount > n)
+            {
+                return impossible;
+            }
+
+            if (onScreenCount == n)
+            {
+                return 0;
+            }
+
+            var ans = impossible;
+
+            if (onScreenCount != bufferCount)
+            {
+                var copyAllCount = onScreenCount != bufferCount
+                    ? recursiveFunc((onScreenCount, onScreenCount))
+                    : impossible;
+
+                if (copyAllCount != impossible)
+                {
+                    ans = Math.Min(ans, copyAllCount + 1);
+                }
+            }
+
+            if (bufferCount <= 0)
+            {
+                return ans;
+            }
+
+            var pasteCount = recursiveFunc((onScreenCount + bufferCount, bufferCount));
+
+            if (pasteCount != impossible)
+            {
+                ans = Math.Min(ans, pasteCount + 1);
+            }
+
+            return ans;
+        });
+
+        return dp.GetOrCalculate((1, 0));
+    }
+
+    private class DynamicProgramming<TKey, TValue> where TKey : notnull
+    {
+        private readonly Func<TKey, Func<TKey, TValue>, TValue> _func;
+        private readonly Dictionary<TKey, TValue> _cache = new();
+
+        public DynamicProgramming(Func<TKey, Func<TKey, TValue>, TValue> func) => _func = func;
+
+        public TValue GetOrCalculate(TKey key) => !_cache.TryGetValue(key, out var value)
+            ? _cache[key] = _func(key, GetOrCalculate)
+            : value;
+    }
+}

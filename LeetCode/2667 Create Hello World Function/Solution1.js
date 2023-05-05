a6824a44d83f1f50af10f41647dc875b3a2da98d
Michael Naumov <mnaoumov@gmail.com>
2023-05-05 16:01:31 -0600

    2667 Create Hello World Function

---
+++ LeetCode/2667 Create Hello World Function/Solution1.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/submissions/detail/944639030/
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function () {
+    return function (...args) {
+        return "Hello World";
+    }
+};
---
+++ LeetCode/2667 Create Hello World Function/Tests.cs
@@ -0,0 +1,8 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2667_Create_Hello_World_Function;
+
+[UsedImplicitly]
+public class Tests : JavaScriptTestsBase<Tests>
+{
+}

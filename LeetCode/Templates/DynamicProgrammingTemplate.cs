26387aa5a91eff2b37d89377b045091505d14131
Michael Naumov <mnaoumov@gmail.com>
2023-05-05 20:58:17 -0600

    Add DynamicProgramming sample

--- LeetCode/Templates/DynamicProgrammingTemplate.cs
+++ LeetCode/Templates/DynamicProgrammingTemplate.cs
@@ -3,11 +3,23 @@
 // ReSharper disable UnusedType.Global
 // ReSharper disable UnusedMember.Local
 // ReSharper disable UnusedType.Local
+// ReSharper disable UnusedVariable
+// ReSharper disable UnusedParameter.Local
+#pragma warning disable IDE0051
 
 namespace LeetCode.Templates;
 
 public static class DynamicProgrammingTemplate
 {
+    private static void Sample()
+    {
+        var dp = new DynamicProgramming<(int a, int b), int>((key, recursiveFunc) =>
+        {
+            var (a, b) = key;
+            return 0;
+        });
+    }
+
     private class DynamicProgramming<TKey, TValue> where TKey : notnull
     {
         private readonly Func<TKey, Func<TKey, TValue>, TValue> _func;

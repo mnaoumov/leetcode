7ab071df7d8353dd24fbd8967aa6f22f86541865
Michael Naumov <mnaoumov@gmail.com>
2023-05-07 20:07:19 -0600

    0369 Plus One Linked List

---
+++ LeetCode/0369 Plus One Linked List/Solution1.cs
@@ -0,0 +1,45 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0369_Plus_One_Linked_List;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/946352974/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public ListNode PlusOne(ListNode head)
+    {
+        var node = head;
+
+        ListNode? lastNotNineNode = null;
+        var ans = head;
+
+        while (node != null)
+        {
+            if (node.val != 9)
+            {
+                lastNotNineNode = node;
+            }
+
+            node = node.next;
+        }
+
+        if (lastNotNineNode == null)
+        {
+            ans = new ListNode(0, head);
+            lastNotNineNode = ans;
+        }
+
+        lastNotNineNode.val++;
+        node = lastNotNineNode.next;
+
+        while (node != null)
+        {
+            node.val = 0;
+            node = node.next;
+        }
+
+        return ans;
+    }
+}

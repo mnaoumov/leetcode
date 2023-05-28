d73a81fcf4ea1c736d9cc2c0b4fdaa8229168a54
Michael Naumov <mnaoumov@gmail.com>
2023-05-27 19:15:02 -0600

    1510 Stone Game IV

---
+++ LeetCode/1510 Stone Game IV/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._1510_Stone_Game_IV;
+
+[PublicAPI]
+public interface ISolution
+{
+    public bool WinnerSquareGame(int n);
+}
---
+++ LeetCode/1510 Stone Game IV/Solution1.cs
@@ -0,0 +1,38 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._1510_Stone_Game_IV;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/958657548/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public bool WinnerSquareGame(int n)
+    {
+        var dp = new DynamicProgramming<int, bool>((m, recursiveFunc) =>
+        {
+            if (m == 0)
+            {
+                return false;
+            }
+
+            var sqrt = (int) Math.Sqrt(m);
+            return Enumerable.Range(1, sqrt).Any(k => !recursiveFunc(m - k * k));
+        });
+
+        return dp.GetOrCalculate(n);
+    }
+
+    private class DynamicProgramming<TKey, TValue> where TKey : notnull
+    {
+        private readonly Func<TKey, Func<TKey, TValue>, TValue> _func;
+        private readonly Dictionary<TKey, TValue> _cache = new();
+
+        public DynamicProgramming(Func<TKey, Func<TKey, TValue>, TValue> func) => _func = func;
+
+        public TValue GetOrCalculate(TKey key) => !_cache.TryGetValue(key, out var value)
+            ? _cache[key] = _func(key, GetOrCalculate)
+            : value;
+    }
+}

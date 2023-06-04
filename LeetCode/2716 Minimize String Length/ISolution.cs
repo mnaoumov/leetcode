43dccb130a08272af6ae652b88681a83bbd40a0c
Michael Naumov <mnaoumov@gmail.com>
2023-06-04 10:16:59 -0600

    2716 Minimize String Length

---
+++ LeetCode/2716 Minimize String Length/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2716_Minimize_String_Length;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int MinimizedStringLength(string s);
+}
---
+++ LeetCode/2716 Minimize String Length/Solution1.cs
@@ -0,0 +1,12 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2716_Minimize_String_Length;
+
+/// <summary>
+/// https://leetcode.com/contest/weekly-contest-348/submissions/detail/963320611/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int MinimizedStringLength(string s) => s.ToHashSet().Count;
+}

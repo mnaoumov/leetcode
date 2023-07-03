1b583e81b492964fc2caa96676054637a429b276
Michael Naumov <mnaoumov@gmail.com>
2023-07-02 20:02:01 -0600

    2761 Prime Pairs With Target Sum

---
+++ LeetCode/2761 Prime Pairs With Target Sum/Solution5.cs
@@ -0,0 +1,49 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2761_Prime_Pairs_With_Target_Sum;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/984923347/
+/// </summary>
+[UsedImplicitly]
+[SkipSolution(SkipSolutionReason.TimeLimitExceeded)]
+public class Solution5 : ISolution
+{
+    public IList<IList<int>> FindPrimePairs(int n)
+    {
+        if (n <= 3)
+        {
+            return new List<IList<int>>();
+        }
+
+        var primes = new SortedSet<int>(Enumerable.Range(2, n - 3));
+
+        var lastPrime = 1;
+        var lastToCheck = (int) Math.Sqrt(n - 2);
+
+        while (true)
+        {
+            var p = primes.GetViewBetween(lastPrime + 1, n - 2).Min;
+
+            if (p > lastToCheck)
+            {
+                break;
+            }
+
+            for (var k = p * p; k <= n - 2; k += p)
+            {
+                primes.Remove(k);
+            }
+
+            lastPrime = p;
+        }
+
+        return
+            (from x in primes.TakeWhile(x => x <= n / 2)
+                let y = n - x
+                where primes.Contains(y)
+                select new[] { x, y })
+            .Cast<IList<int>>()
+            .ToArray();
+    }
+}
---
+++ LeetCode/2761 Prime Pairs With Target Sum/Solution6.cs
@@ -0,0 +1,61 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2761_Prime_Pairs_With_Target_Sum;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/984927478/
+/// </summary>
+[UsedImplicitly]
+public class Solution6 : ISolution
+{
+    public IList<IList<int>> FindPrimePairs(int n)
+    {
+        if (n <= 3)
+        {
+            return new List<IList<int>>();
+        }
+
+        var primes = Enumerable.Range(0, n - 1).Select(_ => true).ToArray();
+        primes[0] = false;
+        primes[1] = false;
+
+        var m = (int) Math.Sqrt(n - 2);
+
+        for (var i = 2; i <= m; i++)
+        {
+            if (!primes[i])
+            {
+                continue;
+            }
+
+            for (var j = i * i; j <= n - 2; j += i)
+            {
+                primes[j] = false;
+            }
+        }
+
+        var ans = new List<IList<int>>();
+
+        if (n % 2 == 1)
+        {
+            if (primes[n - 2])
+            {
+                ans.Add(new[] { 2, n - 2 });
+            }
+
+            return ans;
+        }
+
+        for (var x = 2; x <= n / 2; x++)
+        {
+            var y = n - x;
+
+            if (primes[x] && primes[y])
+            {
+                ans.Add(new[] { x, y });
+            }
+        }
+
+        return ans;
+    }
+}

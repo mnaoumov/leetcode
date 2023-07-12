24d2e8f91d684eca7894df0367725d9001a6d7e1
Michael Naumov <mnaoumov@gmail.com>
2023-07-11 21:07:12 -0600

    2765 Longest Alternating Subarray

---
+++ LeetCode/2765 Longest Alternating Subarray/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2765_Longest_Alternating_Subarray;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int AlternatingSubarray(int[] nums);
+}

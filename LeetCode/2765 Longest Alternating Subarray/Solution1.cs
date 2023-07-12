3f4b846c41d5d363df8b7978698ed82d4b9a7ccd
Michael Naumov <mnaoumov@gmail.com>
2023-07-11 21:06:07 -0600

    2765 Longest Alternating Subarray

---
+++ LeetCode/2765 Longest Alternating Subarray/Solution1.cs
@@ -0,0 +1,38 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2765_Longest_Alternating_Subarray;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/989375803/
+/// </summary>
+[UsedImplicitly]
+[SkipSolution(SkipSolutionReason.WrongAnswer)]
+public class Solution1 : ISolution
+{
+    public int AlternatingSubarray(int[] nums)
+    {
+        var ans = 0;
+
+        var plusLength = 0;
+        var minusLength = 1;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            switch (nums[i] - nums[i - 1])
+            {
+                case -1:
+                    minusLength = plusLength + 1;
+                    plusLength = 0;
+                    ans = Math.Max(ans, minusLength);
+                    break;
+                case 1:
+                    plusLength = minusLength + 1;
+                    minusLength = 1;
+                    ans = Math.Max(ans, plusLength);
+                    break;
+            }
+        }
+
+        return ans;
+    }
+}
---
+++ LeetCode/2765 Longest Alternating Subarray/Solution3.cs
@@ -0,0 +1,44 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2765_Longest_Alternating_Subarray;
+
+/// <summary>
+/// https://leetcode.com/contest/biweekly-contest-108/submissions/detail/989385713/
+/// </summary>
+[UsedImplicitly]
+public class Solution3 : ISolution
+{
+    public int AlternatingSubarray(int[] nums)
+    {
+        var ans = 0;
+
+        const int initialPlusLength = 0;
+        const int initialMinusLength = 1;
+
+        var plusLength = initialPlusLength;
+        var minusLength = initialMinusLength;
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            switch (nums[i] - nums[i - 1])
+            {
+                case -1:
+                    minusLength = plusLength + 1;
+                    plusLength = initialPlusLength;
+                    ans = Math.Max(ans, minusLength);
+                    break;
+                case 1:
+                    plusLength = minusLength + 1;
+                    minusLength = initialMinusLength;
+                    ans = Math.Max(ans, plusLength);
+                    break;
+                default:
+                    plusLength = initialPlusLength;
+                    minusLength = initialMinusLength;
+                    break;
+            }
+        }
+
+        return ans < 2 ? -1 : ans;
+    }
+}

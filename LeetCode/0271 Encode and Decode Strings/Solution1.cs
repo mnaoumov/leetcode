14c0c2a7333bf25a69ad1953f5d903b002d35272
Michael Naumov <mnaoumov@gmail.com>
2023-08-07 19:46:29 -0600

    namespace

--- LeetCode/0271 Encode and Decode Strings/Solution1.cs
+++ LeetCode/0271 Encode and Decode Strings/Solution1.cs
@@ -1,26 +1,28 @@
-namespace LeetCode._0271_Encode_and_Decode_Strings
+using JetBrains.Annotations;
+
+namespace LeetCode._0271_Encode_and_Decode_Strings;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/1015202483/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
 {
-    /// <summary>
-    /// https://leetcode.com/submissions/detail/1015202483/
-    /// </summary>
-    public class Solution1 : ISolution
+    public ICodec Create() => new Codec();
+
+    private class Codec : ICodec
     {
-        public ICodec Create() => new Codec();
-
-        private class Codec : ICodec
+        public string encode(IList<string> strs)
         {
-            public string encode(IList<string> strs)
-            {
-                var maxLength = strs.Max(str => str.Length);
-                var separator = new string(' ', maxLength + 1) + '\n';
-                return string.Concat(strs.Select(str => separator + str));
-            }
+            var maxLength = strs.Max(str => str.Length);
+            var separator = new string(' ', maxLength + 1) + '\n';
+            return string.Concat(strs.Select(str => separator + str));
+        }
 
-            public IList<string> decode(string s)
-            {
-                var separator = s.Split('\n')[0] + '\n';
-                return s.Split(separator).Skip(1).ToArray();
-            }
+        public IList<string> decode(string s)
+        {
+            var separator = s.Split('\n')[0] + '\n';
+            return s.Split(separator).Skip(1).ToArray();
         }
     }
 }

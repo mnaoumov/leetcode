6fcbca08652b013b0c3c0a5db5464bb11b3fbf74
Michael Naumov <mnaoumov@gmail.com>
2023-09-20 19:03:56 -0600

    1658 Minimum Operations to Reduce X to Zero

---
+++ LeetCode/1658 Minimum Operations to Reduce X to Zero/Solution1.cs
@@ -0,0 +1,57 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._1658_Minimum_Operations_to_Reduce_X_to_Zero;
+
+/// <summary>
+/// TODO url
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int MinOperations(int[] nums, int x)
+    {
+        var leftCapacityIndexMap = new Dictionary<int, int>
+        {
+            [x] = -1
+        };
+
+        var leftCapacity = x;
+
+        var n = nums.Length;
+
+        for (var i = 0; i < n; i++)
+        {
+            leftCapacity -= nums[i];
+
+            if (leftCapacity < 0)
+            {
+                break;
+            }
+
+            leftCapacityIndexMap[leftCapacity] = i;
+        }
+
+        var ans = int.MaxValue;
+
+        var rightCapacity = x;
+
+        for (var i = 0; i < n; i++)
+        {
+            var rightIndex = n - i;
+
+            if (leftCapacityIndexMap.TryGetValue(x - rightCapacity, out var leftIndex) && leftIndex < rightIndex)
+            {
+                ans = Math.Min(ans, leftIndex + i + 1);
+            }
+
+            rightCapacity -= nums[rightIndex - 1];
+
+            if (rightCapacity < 0)
+            {
+                break;
+            }
+        }
+
+        return ans == int.MaxValue ? -1 : ans;
+    }
+}
---
+++ LeetCode/1658 Minimum Operations to Reduce X to Zero/Tests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using JetBrains.Annotations;
+
+namespace LeetCode._1658_Minimum_Operations_to_Reduce_X_to_Zero;
+
+[UsedImplicitly]
+public class Tests : TestsBase<ISolution, Tests.TestCase>
+{
+    protected override void TestImpl(ISolution solution, TestCase testCase)
+    {
+        Assert.That(solution.MinOperations(testCase.Nums, testCase.X), Is.EqualTo(testCase.Output));
+    }
+
+    public class TestCase : TestCaseBase
+    {
+        public int[] Nums { get; [UsedImplicitly] init; } = null!;
+        public int X { get; [UsedImplicitly] init; }
+        public int Output { get; [UsedImplicitly] init; }
+    }
+}

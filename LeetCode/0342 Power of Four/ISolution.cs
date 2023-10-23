8d5ef0f430acebd6186f1a6fcdb1823da4efe98f
Michael Naumov <mnaoumov@gmail.com>
2023-10-22 18:53:36 -0600

    0342 Power of Four

---
+++ LeetCode/0342 Power of Four/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0342_Power_of_Four;
+
+[PublicAPI]
+public interface ISolution
+{
+    public bool IsPowerOfFour(int n);
+}
---
+++ LeetCode/0342 Power of Four/Solution1.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0342_Power_of_Four;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/1081712202/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public bool IsPowerOfFour(int n) => IsSquare(n) && IsPowerOfTwo((int) Math.Sqrt(n));
+
+    private static bool IsSquare(int n)
+    {
+        if (n < 0)
+        {
+            return false;
+        }
+
+        var m = (int) Math.Sqrt(n);
+        return m * m == n;
+    }
+
+    private static bool IsPowerOfTwo(int n) => n > 0 && (n & n - 1) == 0;
+}

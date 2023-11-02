b20ef77334ed9d40342a3be144258ea61e3217ea
Michael Naumov <mnaoumov@gmail.com>
2023-11-01 20:13:05 -0600

    2265 Count Nodes Equal to Average of Subtree

---
+++ LeetCode/2265 Count Nodes Equal to Average of Subtree/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2265_Count_Nodes_Equal_to_Average_of_Subtree;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int AverageOfSubtree(TreeNode root);
+}
---
+++ LeetCode/2265 Count Nodes Equal to Average of Subtree/Solution1.cs
@@ -0,0 +1,33 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2265_Count_Nodes_Equal_to_Average_of_Subtree;
+
+/// <summary>
+/// TODO url
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int AverageOfSubtree(TreeNode root) => Dfs(root).countNodesEqualToAverage;
+
+    private static (int sum, int subtreeCount, int countNodesEqualToAverage) Dfs(TreeNode? node)
+    {
+        if (node == null)
+        {
+            return (0, 0, 0);
+        }
+
+        var left = Dfs(node.left);
+        var right = Dfs(node.right);
+
+        var sum = left.sum + right.sum + node.val;
+        var subtreeCount = left.subtreeCount + right.subtreeCount + 1;
+        var average = sum / subtreeCount;
+        var countNodesEqualToAverage =
+            left.countNodesEqualToAverage
+            + right.countNodesEqualToAverage
+            + (average == node.val ? 1 : 0);
+        return (sum, subtreeCount, countNodesEqualToAverage);
+
+    }
+}

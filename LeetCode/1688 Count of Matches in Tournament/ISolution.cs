ffcf2ab1dc7bf183e8c23c5de67aa1b49897e2cc
Michael Naumov <mnaoumov@gmail.com>
2023-12-09 15:17:41 -0600

    1688 Count of Matches in Tournament

---
+++ LeetCode/1688 Count of Matches in Tournament/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._1688_Count_of_Matches_in_Tournament;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int NumberOfMatches(int n);
+}
---
+++ LeetCode/1688 Count of Matches in Tournament/Solution1.cs
@@ -0,0 +1,12 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._1688_Count_of_Matches_in_Tournament;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/1113238116/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int NumberOfMatches(int n) => n - 1;
+}

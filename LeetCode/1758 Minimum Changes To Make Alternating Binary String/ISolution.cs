4bd1542bd42123635cada8c52c8a5c633a1b0bb3
Michael Naumov <mnaoumov@gmail.com>
2023-12-23 19:49:41 -0600

    1758 Minimum Changes To Make Alternating Binary String

---
+++ LeetCode/1758 Minimum Changes To Make Alternating Binary String/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._1758_Minimum_Changes_To_Make_Alternating_Binary_String;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int MinOperations(string s);
+}
---
+++ LeetCode/1758 Minimum Changes To Make Alternating Binary String/Solution1.cs
@@ -0,0 +1,26 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._1758_Minimum_Changes_To_Make_Alternating_Binary_String;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/1126992195/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int MinOperations(string s)
+    {
+        var leadingZeroChangesCount = 0;
+
+        for (var i = 0; i < s.Length; i++)
+        {
+            var leadingZeroDigit = i % 2 == 0 ? '0' : '1';
+            if (s[i] != leadingZeroDigit)
+            {
+                leadingZeroChangesCount++;
+            }
+        }
+
+        return Math.Min(leadingZeroChangesCount, s.Length - leadingZeroChangesCount);
+    }
+}

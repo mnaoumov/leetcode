e659c1071f783dfe76eb1862aefbac49e4f3a531
Michael Naumov <mnaoumov@gmail.com>
2023-12-23 19:50:20 -0600

    Fix warnings

--- LeetCode/2353 Design a Food Rating System/Tests.cs
+++ LeetCode/2353 Design a Food Rating System/Tests.cs
@@ -3,6 +3,4 @@ using JetBrains.Annotations;
 namespace LeetCode._2353_Design_a_Food_Rating_System;
 
 [UsedImplicitly]
-public class Tests : SutTestsBase<ISolution, IFoodRatings>
-{
-}
+public class Tests : SutTestsBase<ISolution, IFoodRatings>;

163ae9c6216bade535e8571f949ea9ba01ad26ef
Michael Naumov <mnaoumov@gmail.com>
2024-03-04 09:36:40 -0600

    Daily task 2024-02-28

---
+++ LeetCode/0513 Find Bottom Left Tree Value/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0513_Find_Bottom_Left_Tree_Value;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int FindBottomLeftValue(TreeNode root);
+}
---
+++ LeetCode/0513 Find Bottom Left Tree Value/Solution1.cs
@@ -0,0 +1,45 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._0513_Find_Bottom_Left_Tree_Value;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/1188867854/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int FindBottomLeftValue(TreeNode root)
+    {
+        var queue = new Queue<TreeNode>();
+        queue.Enqueue(root);
+
+        var ans = 0;
+
+        while (queue.Count > 0)
+        {
+            var count = queue.Count;
+
+            for (var i = 0; i < count; i++)
+            {
+                var node = queue.Dequeue();
+
+                if (i == 0)
+                {
+                    ans = node.val;
+                }
+
+                if (node.left != null)
+                {
+                    queue.Enqueue(node.left);
+                }
+
+                if (node.right != null)
+                {
+                    queue.Enqueue(node.right);
+                }
+            }
+        }
+
+        return ans;
+    }
+}

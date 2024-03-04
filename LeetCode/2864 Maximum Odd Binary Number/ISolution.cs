0c3d2eb960749dd1eeba8cd6c0aa09a76b5005b5
Michael Naumov <mnaoumov@gmail.com>
2024-03-04 09:44:35 -0600

    Daily task 2024-03-01

---
+++ LeetCode/2864 Maximum Odd Binary Number/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2864_Maximum_Odd_Binary_Number;
+
+[PublicAPI]
+public interface ISolution
+{
+    public string MaximumOddBinaryNumber(string s);
+}
---
+++ LeetCode/2864 Maximum Odd Binary Number/Solution1.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+
+namespace LeetCode._2864_Maximum_Odd_Binary_Number;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/1190168861/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public string MaximumOddBinaryNumber(string s)
+    {
+        var n = s.Length;
+        var m = s.Count(digit => digit == '1');
+        return string.Concat(Enumerable.Repeat('1', m - 1).Concat(Enumerable.Repeat('0', n - m)).Append('1'));
+    }
+}

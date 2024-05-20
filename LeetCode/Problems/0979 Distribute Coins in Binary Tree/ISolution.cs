d241e165fa356a4056d85c8ecdd2b72584a5a17a
Michael Naumov <mnaoumov@gmail.com>
2024-05-19 19:13:27 -0600

    Daily 2024-05-18

---
+++ LeetCode/Problems/0979 Distribute Coins in Binary Tree/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode.Problems._0979_Distribute_Coins_in_Binary_Tree;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int DistributeCoins(TreeNode root);
+}
---
+++ LeetCode/Problems/0979 Distribute Coins in Binary Tree/Solution1.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+
+namespace LeetCode.Problems._0979_Distribute_Coins_in_Binary_Tree;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/1261388250/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int DistributeCoins(TreeNode root) => DistributeCoins2(root).moves;
+
+    private static (int balance, int moves) DistributeCoins2(TreeNode? root)
+    {
+        if (root == null)
+        {
+            return (0, 0);
+        }
+
+        var left = DistributeCoins2(root.left);
+        var right = DistributeCoins2(root.right);
+        var balance = root.val + left.balance + right.balance - 1;
+        return (balance, moves: left.moves + right.moves + Math.Abs(balance));
+    }
+}

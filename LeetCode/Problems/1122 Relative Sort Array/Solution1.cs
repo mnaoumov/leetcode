d987503872eca20541da1406f5d61de04bee9c6b
Michael Naumov <mnaoumov@gmail.com>
2024-06-11 16:03:59 -0600

    Daily Problem:: 2024-06-11

---
+++ LeetCode/Problems/1122 Relative Sort Array/Solution1.cs
@@ -0,0 +1,16 @@
+using JetBrains.Annotations;
+
+namespace LeetCode.Problems._1122_Relative_Sort_Array;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/1285334840/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int[] RelativeSortArray(int[] arr1, int[] arr2)
+    {
+        var indexMap = arr2.Select((num, index)=>(num, index)).ToDictionary(x=>x.num, x=>x.index);
+        return arr1.OrderBy(num => indexMap.TryGetValue(num, out var index) ? (0, index) : (1, num)).ToArray();
+    }
+}
---
+++ LeetCode/Problems/1122 Relative Sort Array/Tests.cs
@@ -0,0 +1,19 @@
+using JetBrains.Annotations;
+
+namespace LeetCode.Problems._1122_Relative_Sort_Array;
+
+[UsedImplicitly]
+public class Tests : TestsBase<ISolution, Tests.TestCase>
+{
+    protected override void TestImpl(ISolution solution, TestCase testCase)
+    {
+        AssertCollectionEqualWithDetails(solution.RelativeSortArray(testCase.Arr1, testCase.Arr2), testCase.Output);
+    }
+
+    public class TestCase : TestCaseBase
+    {
+        public int[] Arr1 { get; [UsedImplicitly] init; } = null!;
+        public int[] Arr2 { get; [UsedImplicitly] init; } = null!;
+        public int[] Output { get; [UsedImplicitly] init; } = null!;
+    }
+}

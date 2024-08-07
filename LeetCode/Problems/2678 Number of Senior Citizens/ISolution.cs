9f3d261e15f60de279f6886a6c43c18e86ee9247
Michael Naumov <mnaoumov@gmail.com>
2024-08-07 08:44:35 -0600

    Daily Problem:: 2024-08-01

---
+++ LeetCode/Problems/2678 Number of Senior Citizens/ISolution.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace LeetCode.Problems._2678_Number_of_Senior_Citizens;
+
+[PublicAPI]
+public interface ISolution
+{
+    public int CountSeniors(string[] details);
+}
---
+++ LeetCode/Problems/2678 Number of Senior Citizens/Solution1.cs
@@ -0,0 +1,18 @@
+using JetBrains.Annotations;
+
+namespace LeetCode.Problems._2678_Number_of_Senior_Citizens;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/1341283468/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int CountSeniors(string[] details)
+    {
+        const int ageStartIndex = 11;
+        const int ageStrLength = 2;
+
+        return details.Count(detail => int.Parse(detail.Substring(ageStartIndex, ageStrLength)) > 60);
+    }
+}

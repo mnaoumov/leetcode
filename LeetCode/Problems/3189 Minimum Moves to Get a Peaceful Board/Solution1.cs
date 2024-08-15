bd1c5faf9d5a8187e2877b2e789bc3df4a132dab
Michael Naumov <mnaoumov@gmail.com>
2024-08-15 13:55:50 -0600

    Weekly Premium Problem:: 2024-08-W3

---
+++ LeetCode/Problems/3189 Minimum Moves to Get a Peaceful Board/Solution1.cs
@@ -0,0 +1,19 @@
+using JetBrains.Annotations;
+
+namespace LeetCode.Problems._3189_Minimum_Moves_to_Get_a_Peaceful_Board;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/1356990019/
+/// </summary>
+[UsedImplicitly]
+public class Solution1 : ISolution
+{
+    public int MinMoves(int[][] rooks)
+    {
+        var orderedByRows = rooks.OrderBy(r => r[0]).ToArray();
+        var orderedByColumns = rooks.OrderBy(r => r[1]).ToArray();
+
+        return orderedByRows.Select((r, i) => Math.Abs(r[0] - i)).Sum() +
+               orderedByColumns.Select((r, i) => Math.Abs(r[1] - i)).Sum();
+    }
+}

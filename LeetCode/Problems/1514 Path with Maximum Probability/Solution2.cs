9a4971b0eb79529267a1fa4ca0f17cf384bf7ff6
Michael Naumov <mnaoumov@gmail.com>
2024-08-31 21:33:57 -0600

    Daily Problem:: 2024-08-31

---
+++ LeetCode/Problems/1514 Path with Maximum Probability/Solution2.cs
@@ -0,0 +1,112 @@
+using JetBrains.Annotations;
+
+namespace LeetCode.Problems._1514_Path_with_Maximum_Probability;
+
+/// <summary>
+/// https://leetcode.com/submissions/detail/1374890990/
+/// </summary>
+[UsedImplicitly]
+public class Solution2 : ISolution
+{
+    public double MaxProbability(int n, int[][] edges, double[] succProb, int start, int end)
+    {
+        var graph = new EdgeWeightedGraph<int>();
+
+        for (var i = 0; i < n; i++)
+        {
+            graph.AddNode(i);
+        }
+
+        for (var i = 0; i < edges.Length; i++)
+        {
+            graph.AddEdge(new Edge<int>(edges[i][0], edges[i][1], -Math.Log2(succProb[i])));
+        }
+
+        var sp = new DijkstraShortestPath<int>(graph, start);
+
+        return Math.Pow(2, -sp.DistanceTo(end));
+    }
+
+    private class DijkstraShortestPath<T> where T : notnull
+    {
+        private readonly Dictionary<T, double> _distances = new();
+
+        public DijkstraShortestPath(EdgeWeightedGraph<T> graph, T source)
+        {
+            _distances[source] = 0;
+            var marked = new HashSet<T>();
+            var unmarked = new PriorityQueue<T, double>();
+            unmarked.Enqueue(source, 0);
+
+            while (unmarked.Count > 0)
+            {
+                var v = unmarked.Dequeue();
+                var dv = DistanceTo(v);
+
+                foreach (var adjEdge in graph.AdjacentEdges(v))
+                {
+                    var w = adjEdge.Other(v);
+
+                    if (marked.Contains(w))
+                    {
+                        continue;
+                    }
+
+                    var dw = DistanceTo(w);
+                    var dw2 = dv + adjEdge.Weight;
+
+                    if (dw2 >= dw)
+                    {
+                        continue;
+                    }
+
+                    _distances[w] = dw2;
+                    unmarked.Enqueue(w, dw2);
+                }
+
+                marked.Add(v);
+            }
+        }
+
+        public double DistanceTo(T target) => _distances.GetValueOrDefault(target, double.PositiveInfinity);
+    }
+
+    private class EdgeWeightedGraph<T> where T : notnull
+    {
+        private readonly Dictionary<T, HashSet<Edge<T>>> _adjacentEdges = new();
+
+        public void AddEdge(Edge<T> edge)
+        {
+            var v = edge.Either();
+            var w = edge.Other(v);
+            AddNode(v);
+            AddNode(w);
+            _adjacentEdges[v].Add(edge);
+            _adjacentEdges[w].Add(edge);
+        }
+
+        public void AddNode(T node) => _adjacentEdges.TryAdd(node, new HashSet<Edge<T>>());
+
+        public IEnumerable<Edge<T>> AdjacentEdges(T node) => _adjacentEdges.GetValueOrDefault(node, new HashSet<Edge<T>>());
+    }
+
+    private record Edge<T>(T Node1, T Node2, double Weight)
+    {
+        public T Either() => Node1;
+
+        public T Other(T node)
+        {
+            if (Equals(node, Node1))
+            {
+                return Node2;
+            }
+
+            if (Equals(node, Node2))
+            {
+                return Node1;
+            }
+
+            throw new InvalidOperationException();
+        }
+    }
+}

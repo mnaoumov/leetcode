b5495baa06055141f813ab620c5c3d892c8512fe
Michael Naumov <mnaoumov@gmail.com>
2025-03-13 19:35:10 -0600

    Daily Problem:: 2025-03-14

--- LeetCode/Problems/3356 Zero Array Transformation II/Solution1.cs
+++ LeetCode/Problems/3356 Zero Array Transformation II/Solution1.cs
@@ -1,14 +1,62 @@
 namespace LeetCode.Problems._3356_Zero_Array_Transformation_II;
 
 /// <summary>
-/// TODO url
+/// https://leetcode.com/problems/zero-array-transformation-ii/submissions/1573040586/
 /// </summary>
 [UsedImplicitly]
-[SkipSolution(SkipSolutionReason.NotImplemented)]
 public class Solution1 : ISolution
 {
     public int MinZeroArray(int[] nums, int[][] queries)
     {
-        throw new NotImplementedException();
+        var n = nums.Length;
+        var m = queries.Length;
+
+        var low = 0;
+        var high = m;
+
+        while (low <= high)
+        {
+            var mid = low + (high - low >> 1);
+
+            if (IsMinZero(mid))
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+
+        return low == m + 1 ? -1 : low;
+
+        bool IsMinZero(int count)
+        {
+            var diffs = new int[n + 1];
+
+            for (var i = 0; i < count; i++)
+            {
+                var query = queries[i];
+                var l = query[0];
+                var r = query[1];
+                var val = query[2];
+                diffs[l] += val;
+                diffs[r + 1] -= val;
+            }
+
+            var prefixSum = 0;
+
+            for (var i = 0; i < n; i++)
+            {
+                prefixSum += diffs[i];
+
+                if (prefixSum < nums[i])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }

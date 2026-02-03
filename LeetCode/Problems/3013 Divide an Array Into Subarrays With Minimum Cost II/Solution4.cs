4e0aa1c0f3e905d48ccf25b74cff1b88bd602bba
Michael Naumov <mnaoumov@gmail.com>
2026-02-02 19:19:15 -0600

    Daily problem 2026-02-02

--- LeetCode/Problems/3013 Divide an Array Into Subarrays With Minimum Cost II/Solution4.cs
+++ LeetCode/Problems/3013 Divide an Array Into Subarrays With Minimum Cost II/Solution4.cs
@@ -1,11 +1,10 @@
 namespace LeetCode.Problems._3013_Divide_an_Array_Into_Subarrays_With_Minimum_Cost_II;
 
 /// <summary>
-/// https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-ii/submissions/1906081458/
+/// https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-ii/submissions/
 /// </summary>
 [UsedImplicitly]
-[SkipSolution(SkipSolutionReason.WrongAnswer)]
-public class Solution3 : ISolution
+public class Solution4 : ISolution
 {
     public long MinimumCost(int[] nums, int k, int dist)
     {
@@ -25,20 +24,21 @@ public class Solution3 : ISolution
             minSum += list[j].num;
         }
 
-        var ans = nums[0] +nums[1] + minSum;
+        var ans = nums[0] + nums[1] + minSum;
 
         for (var i1 = 2; i1 < nums.Length + 2 - k; i1++)
         {
-            var oldKey = (num: nums[i1 - 1], index: i1 - 1);
+            var oldKey = (num: nums[i1], index: i1);
             var index = list.BinarySearch(oldKey);
 
-            if (index >= 0)
-            {
-                list.RemoveAt(index);
+            list.RemoveAt(index);
 
-                if (index < k - 2)
+            if (index < k - 2)
+            {
+                minSum -= oldKey.num;
+
+                if (list.Count > k - 3)
                 {
-                    minSum -= oldKey.num;
                     minSum += list[k - 3].num;
                 }
             }
@@ -51,7 +51,11 @@ public class Solution3 : ISolution
                 if (index < k - 2)
                 {
                     minSum += newKey.num;
-                    minSum -= list[k - 2].num;
+
+                    if (list.Count > k - 2)
+                    {
+                        minSum -= list[k - 2].num;
+                    }
                 }
             }
 

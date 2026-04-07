cce44df383de30d159c38b4ed2896308f5193dbb
Michael Naumov <mnaoumov@gmail.com>
2026-04-07 00:55:44 -0600

    feat: daily problem 2026-03-26

--- LeetCode/Problems/3548 Equal Sum Grid Partition II/Solution1.cs
+++ LeetCode/Problems/3548 Equal Sum Grid Partition II/Solution1.cs
@@ -1,14 +1,108 @@
 namespace LeetCode.Problems._3548_Equal_Sum_Grid_Partition_II;
 
 /// <summary>
-/// TODO url
+/// https://leetcode.com/problems/equal-sum-grid-partition-ii/submissions/1960534390/
 /// </summary>
 [UsedImplicitly]
-[SkipSolution(SkipSolutionReason.NotImplemented)]
 public class Solution1 : ISolution
 {
     public bool CanPartitionGrid(int[][] grid)
     {
-        throw new NotImplementedException();
+        var totalSum = grid.Select(row => row.Select(value => (long) value).Sum()).Sum();
+
+        var currentGrid = grid;
+
+        for (var i = 0; i < 4; i++)
+        {
+            if (CanPartitionGrid2(currentGrid, totalSum))
+            {
+                return true;
+            }
+
+            if (i < 3)
+            {
+                currentGrid = Rotate(currentGrid);
+            }
+        }
+
+        return false;
+    }
+
+    private static int[][] Rotate(int[][] grid)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+
+        var rotated = Enumerable.Range(0, n).Select(_ => new int[m]).ToArray();
+
+        for (var i = 0; i < m; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                rotated[n - 1 - j][i] = grid[i][j];
+            }
+        }
+
+        return rotated;
+    }
+
+    private static bool CanPartitionGrid2(int[][] grid, long totalSum)
+    {
+        var m = grid.Length;
+        var n = grid[0].Length;
+
+        if (m == 1)
+        {
+            return false;
+        }
+
+        var topSum = 0L;
+
+        var topSeen = new HashSet<long>();
+
+        for (var i = 0; i < m - 1; i++)
+        {
+            for (var j = 0; j < n; j++)
+            {
+                var value = grid[i][j];
+                topSeen.Add(value);
+                topSum += value;
+            }
+
+            var valueToRemove = 2 * topSum - totalSum;
+
+            if (valueToRemove == 0)
+            {
+                return true;
+            }
+
+            if (!topSeen.Contains(valueToRemove))
+            {
+                continue;
+            }
+
+            if (i == 0)
+            {
+                if (n > 1 && (grid[0][0] == valueToRemove || grid[0][n - 1] == valueToRemove))
+                {
+                    return true;
+                }
+
+                continue;
+            }
+
+
+            if (n > 1)
+            {
+                return true;
+            }
+
+            if (i > 0 && (grid[0][0] == valueToRemove || grid[i][0] == valueToRemove))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 }

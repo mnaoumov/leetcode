553724b9adc350195f3744e877d9f66bd46b2511
Michael Naumov <mnaoumov@gmail.com>
2026-09-15 23:00:53 -0600

    Daily problem 2026-08-28

--- LeetCode/Problems/3734 Lexicographically Smallest Palindromic Permutation Greater Than Target/Solution1.cs
+++ LeetCode/Problems/3734 Lexicographically Smallest Palindromic Permutation Greater Than Target/Solution1.cs
@@ -1,14 +1,85 @@
+using System.Text;
+
 namespace LeetCode.Problems._3734_Lexicographically_Smallest_Palindromic_Permutation_Greater_Than_Target;
 
 /// <summary>
-/// TODO url
+/// https://leetcode.com/problems/lexicographically-smallest-palindromic-permutation-greater-than-target/submissions/2122516908/
 /// </summary>
 [UsedImplicitly]
-[SkipSolution(SkipSolutionReason.NotImplemented)]
 public class Solution1 : ISolution
 {
     public string LexPalindromicPermutation(string s, string target)
     {
-        throw new NotImplementedException();
+        var letterCounts = s.GroupBy(x => x).ToDictionary(g => g.Key, g => g.Count());
+
+        if (letterCounts.Count(kvp => kvp.Value % 2 == 1) > 1)
+        {
+            return "";
+        }
+
+        const char noLetter = '\0';
+        var oddCountLetter = letterCounts.Keys.FirstOrDefault(k => letterCounts[k] % 2 == 1, noLetter);
+
+        var n = s.Length;
+        var sb = new StringBuilder { Length = n };
+
+        if (oddCountLetter != noLetter)
+        {
+            sb[n / 2] = oddCountLetter;
+            letterCounts[oddCountLetter]--;
+
+            if (letterCounts[oddCountLetter] == 0)
+            {
+                letterCounts.Remove(oddCountLetter);
+            }
+        }
+
+        return CanAssignLetter(0, false) ? sb.ToString() : "";
+
+        bool TryAssignLetter(int index, bool isGreaterAlready, char letter)
+        {
+            sb[index] = letter;
+            sb[n - 1 - index] = letter;
+            letterCounts[letter] -= 2;
+            if (letterCounts[letter] == 0)
+            {
+                letterCounts.Remove(letter);
+            }
+
+            var ans = CanAssignLetter(index + 1, isGreaterAlready);
+
+            if (ans)
+            {
+                return ans;
+            }
+
+            letterCounts.TryAdd(letter, 0);
+            letterCounts[letter] += 2;
+            return ans;
+        }
+
+        bool CanAssignLetter(int index, bool isGreaterAlready)
+        {
+            if (index == n / 2)
+            {
+                return isGreaterAlready || sb.ToString().CompareTo(target, StringComparison.Ordinal) > 0;
+            }
+
+
+            if (isGreaterAlready)
+            {
+                return TryAssignLetter(index, isGreaterAlready, letterCounts.Keys.Min());
+            }
+
+            var targetLetter = target[index];
+
+            if (letterCounts.ContainsKey(targetLetter) && TryAssignLetter(index, false, targetLetter))
+            {
+                return true;
+            }
+
+            var nextLetter = letterCounts.Keys.Where(l => l > targetLetter).DefaultIfEmpty(noLetter).Min();
+            return nextLetter != noLetter && TryAssignLetter(index, true, nextLetter);
+        }
     }
 }
--- LeetCode/Problems/3734 Lexicographically Smallest Palindromic Permutation Greater Than Target/Tests.cs
+++ LeetCode/Problems/3734 Lexicographically Smallest Palindromic Permutation Greater Than Target/Tests.cs
@@ -1,7 +1,6 @@
 namespace LeetCode.Problems._3734_Lexicographically_Smallest_Palindromic_Permutation_Greater_Than_Target;
 
 [UsedImplicitly]
-[Category("TODO")]
 public class Tests : TestsBase<ISolution, Tests.TestCase>
 {
     protected override void TestCore(ISolution solution, TestCase testCase)
